cb8bc3762415985dc3073eca5409aafd6941ce77
xin wei <xin_wei@intuit.com>
2017-03-24 12:22:38 -0700

    4sum , o(n^3)

---
+++ learnpythonthehardway/4sum.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        for i in xrange(len(nums) - 3):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in xrange(i + 1, len(nums) - 2):
+                if j != i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j + 1, len(nums) - 1
+                while left < right:
+                    if nums[left] + nums[right] == sum:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+
+                        """
+                        remove duplicate
+                        """
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[left] + nums[right] < sum:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return res
+
+
+
+
+
+if __name__ == '__main__':
+    result = Solution().fourSum([1, 0, -1, 0, -2, 2, 1, -1, 2, -2], 0)
+    print result
