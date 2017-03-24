a5e1143d69b129a8530579ffe9a161de4c733f57
xin wei <xin_wei@intuit.com>
2017-03-24 12:58:12 -0700

    4sum , o(n^2) with map

--- learnpythonthehardway/4sum.py
+++ learnpythonthehardway/4sum.py
@@ -1,3 +1,4 @@
+import collections
 class Solution(object):
     def fourSum(self, nums, target):
         """
@@ -36,9 +37,34 @@ class Solution(object):
         return res
 
 
+# Time:  O(n^2 * p) ~ O(n^4)
+# Space: O(n^2)
+class Solution3(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums, result, lookup = sorted(nums), [], collections.defaultdict(list)
+        for i in xrange(0, len(nums) - 1):
+            for j in xrange(i + 1, len(nums)):
+                lookup[nums[i] + nums[j]].append([i, j])
 
+        for i in lookup.keys():
+            if target - i in lookup:
+                for x in lookup[i]:
+                    for y in lookup[target - i]:
+                        [a, b], [c, d] = x, y
+                        if a is not c and a is not d and b is not c and b is not d:
+                            quad = sorted([nums[a], nums[b], nums[c], nums[d]])
+                            if quad not in result:
+                                result.append(quad)
+        return sorted(result)
 
 
 if __name__ == '__main__':
     result = Solution().fourSum([1, 0, -1, 0, -2, 2, 1, -1, 2, -2], 0)
+    print result
+    result = Solution3().fourSum([1, 0, -1, 0, -2, 2, 1, -1, 2, -2], 0)
     print result
