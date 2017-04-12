b67ea610cfa5ba9aae7f0c18f148526622661b62
xin wei <xin_wei@intuit.com>
2017-04-11 22:53:35 -0700

    falttern by using post fix traversal.

---
+++ learnpythonthehardway/flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,62 @@
+# Time:  O(n)
+# Space: O(h), h is height of binary tree
+#
+# Given a binary tree, flatten it to a linked list in-place.
+#
+# For example,
+# Given
+#
+#          1
+#         / \
+#        2   5
+#       / \   \
+#      3   4   6
+# The flattened tree should look like:
+#    1
+#     \
+#      2
+#       \
+#        3
+#         \
+#          4
+#           \
+#            5
+#             \
+#              6
+#
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return nothing, do it in place
+    prev = None
+    def flatten(self, root):
+        if root != None:
+            self.flatten(root.right)
+            self.flatten(root.left)
+            root.right = self.prev
+            root.left = None
+            self.prev = root
+            return root
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right = TreeNode(5)
+    root.right.right = TreeNode(6)
+    result = Solution().flatten(root)
+    print result.val
+    print result.right.val
+    print result.right.right.val
+    print result.right.right.right.val
+    print result.right.right.right.right.val
+    print result.right.right.right.right.right.val
+
