cc63d70db45409173656affc458442c104c5fc34
xin wei <xin_wei@intuit.com>
2017-05-10 16:55:51 -0700

    super cool

---
+++ learnpythonthehardway/minimum-moves-to-equal-array-elements-ii.py
@@ -0,0 +1,12 @@
+# Time:  O(n) on average
+# Space: O(1)
+
+class Solution(object):
+    def minMoves2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        median = sorted(nums)[len(nums)/2]
+        return sum(abs(num - median) for num in nums)
+
