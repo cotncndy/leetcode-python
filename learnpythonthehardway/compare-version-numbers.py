c4cc42817b2e124ea9584581868b25efcb23c4c8
xin wei <xin_wei@intuit.com>
2017-05-16 11:28:06 -0700

    different methods

--- learnpythonthehardway/compare-version-numbers.py
+++ learnpythonthehardway/compare-version-numbers.py
@@ -43,3 +43,26 @@ class Solution(object):
             j += 1
 
         return 0
+
+    def compareVersion2(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        v1, v2 = version1.split("."), version2.split(".")
+        i, j = 0, 0
+        while i < len(v1) and j < len(v2):
+            if int(v1[i]) != int(v2[j]):
+                return 1 if int(v1[i]) > int(v2[j]) else -1
+
+            i, j = i + 1, j + 1
+
+        while i < len(v1):
+            if int(v1[i]) != '0':
+                return 1
+        while j < len(v2):
+            if int(v2[j]) != '0':
+                return -1
+
+        return 0
