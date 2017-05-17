0202011fd4b37ee0859a04d489b63690767e236d
xin wei <xin_wei@intuit.com>
2017-05-17 16:11:32 -0700

    fixed.

--- learnpythonthehardway/one-edit-distance.py
+++ learnpythonthehardway/one-edit-distance.py
@@ -12,17 +12,23 @@ class Solution(object):
         :rtype: bool
         """
         m, n = len(s), len(t)
-        if abs(m - n) == 1:
-            return True
-        elif abs(m - n) > 1:
-            return False
-        else:
-            i = 0
-            for k in xrange(len(s)):
-                if s[k] != t[k]:
-                    i += 1
-            return i == 1
+        for i in xrange(min(m, n)):
+            if (s[i] != t[i]):
+                if m > n:
+                    return s[i + 1::] == t[i::]
+                elif n > m:
+                    return s[i::] == t[i + 1::]
+                else:
+                    return s[i + 1::] == t[i + 1::]
+
+        return abs(m - n) == 1
+
+
 
 
 if __name__ == "__main__":
     print Solution().isOneEditDistance("teacher", "acher")
+    print Solution().isOneEditDistance("teacher", "tacher")
+    print Solution().isOneEditDistance("teacher", "tEacher")
+    print Solution().isOneEditDistance("teacher", "teacher")
+    print Solution().isOneEditDistance("teacher", "teache")
