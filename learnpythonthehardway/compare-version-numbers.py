d79794a0814d523f73a9273a36b8a34dd707e9b4
xin wei <xin_wei@intuit.com>
2017-05-16 23:05:04 -0700

    slowest methods, 77ms

--- learnpythonthehardway/compare-version-numbers.py
+++ learnpythonthehardway/compare-version-numbers.py
@@ -90,3 +90,19 @@ class Solution(object):
                 i += 1
 
         return 0
+
+    def compareVersion4(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+
+        v1 = [int(x) for x in version1.split(".")]
+        v2 = [int(x) for x in version2.split(".")]
+        while len(v1) != len(v2):
+            if len(v1) > len(v2):
+                v2.append(0)
+            else:
+                v1.append(0)
+        return cmp(v1, v2)
