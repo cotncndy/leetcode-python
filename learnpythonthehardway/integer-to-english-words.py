8b3f8bbfa69b08b7bdbf4cebbf93fac1f6fd6a26
xin wei <xin_wei@intuit.com>
2017-05-19 12:40:02 -0700

    Submission Result: Wrong Answer More Details
    Input:
    123
    Output:
    "One Hundred Twenty Three "
    Expected:
    "One Hundred Twenty Three"
    
    notice the space at the end of line

---
+++ learnpythonthehardway/integer-to-english-words.py
@@ -0,0 +1,77 @@
+# Time:  O(logn), n is the value of the integer
+# Space: O(1)
+#
+# Convert a non-negative integer to its english words representation.
+# Given input is guaranteed to be less than 2^31 - 1.
+#
+# For example,
+# 123 -> "One Hundred Twenty Three"
+# 12345 -> "Twelve Thousand Three Hundred Forty Five"
+# 1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+#
+# Hint:
+#
+# 1. Did you see a pattern in dividing the number into chunk of words?
+# For example, 123 and 123000.
+#
+# 2. Group the number by thousands (3 digits). You can write a helper
+# function that takes a number less than 1000 and convert just that chunk to words.
+#
+# 3. There are many edge cases. What are some good test cases?
+# Does your code work with input such as 0? Or 1000010?
+# (middle chunk is zero and should not be printed out)
+#
+
+class Solution(object):
+    def numberToWords(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "Zero"
+
+        def threeDigits(n):
+            if n / 100:
+                if n % 100:
+                    s = lookup[n / 100] + " Hundred " + twoDigits(n % 100)
+                else:
+                    s = lookup[n / 100] + " Hundred "
+            else:
+                s = twoDigits(n)
+
+            return s
+
+        def twoDigits(n):
+            if n in lookup:
+                return lookup[n]
+
+            return lookup[n / 10 * 10] + " " + lookup[n % 10]
+
+        lookup = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", \
+                  5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", \
+                  10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", \
+                  15: "Fifteen", 16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", \
+                  20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", \
+                  70: "Seventy", 80: "Eighty", 90: "Ninety"}
+
+        unit = ["", "Thousand", "Million", "Billion"]
+
+        res, i = [], 0
+        while num:
+            cur = num % 1000
+            if cur:
+                res.append(threeDigits(cur) + " " + unit[i])
+
+            num /= 1000
+            i += 1
+
+        return " ".join(res[::-1])
+
+
+if __name__ == "__main__":
+    print Solution().numberToWords(1234567)
+    print Solution().numberToWords(1000010)
+    print Solution().numberToWords(100003103)
+    print Solution().numberToWords(100)
+    print Solution().numberToWords(123)
