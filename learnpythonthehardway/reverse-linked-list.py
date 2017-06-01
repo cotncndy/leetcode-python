0600442903f7fb1f167a1375af2b8e1c8e9c273c
xin wei <xin_wei@intuit.com>
2017-06-01 15:21:58 -0700

    reverse linked list

---
+++ learnpythonthehardway/reverse-linked-list.py
@@ -0,0 +1,41 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Reverse a singly linked list.
+#
+# click to show more hints.
+#
+# Hint:
+# A linked list can be reversed either iteratively or recursively. Could you implement both?
+#
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, repr(self.next))
+
+
+# Iterative solution.
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        dummy = ListNode(float("-inf"))
+        while head:
+            dummy.next, head.next, head = head, dummy.next, head.next
+
+        return dummy.next
+
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    print Solution().reverseList(head)
