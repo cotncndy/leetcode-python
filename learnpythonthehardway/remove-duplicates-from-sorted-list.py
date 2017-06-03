7443add3c54f7c6bc360e7b8b5e36ff5b3beddd8
xin wei <xin_wei@intuit.com>
2017-06-03 14:03:30 -0700

    remove duplicates I

---
+++ learnpythonthehardway/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,34 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+#
+# For example,
+# Given 1->1->2, return 1->2.
+# Given 1->1->2->3->3, return 1->2->3.
+#
+
+# Definition for singly-linked list.
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        cur = head
+        while cur:
+            runner = cur.next
+            while runner and runner.val == cur.val:
+                runner = runner.next
+            cur.next = runner
+            cur = runner
+
+        return head
