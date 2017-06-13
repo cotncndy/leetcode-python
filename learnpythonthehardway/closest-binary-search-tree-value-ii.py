98648d7a633ec169a5180e268a7734ab3c422cae
xin wei <xin_wei@intuit.com>
2017-06-12 17:16:21 -0700

    accepted

---
+++ learnpythonthehardway/closest-binary-search-tree-value-ii.py
@@ -0,0 +1,37 @@
+# Time:  O(h + k)
+# Space: O(h)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def closestKValues(self, root, target, k):
+        """
+        :type root: TreeNode
+        :type target: float
+        :type k: int
+        :rtype: List[int]
+        """
+        res = []
+        self.inorder(root, target, k, res)
+
+        return res
+
+    def inorder(self, root, target, k, res):
+        if not root:
+            return
+        self.inorder(root, target, k, res)
+        if len(res) < k:
+            res.append(root.val)
+        else:
+            if abs(target - res[0]) > abs(target - root.val):
+                res.pop(0)
+                res.append(root.val)
+            else:
+                return
+        self.inorder(root, target, k, res)
