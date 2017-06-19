fab77b3aaa5e2993f9a95f7d9f4806a09965549f
xin wei <xin_wei@intuit.com>
2017-06-19 05:24:35 -0700

    ??? need to figure out how this works

---
+++ learnpythonthehardway/super-ugly-number.py
@@ -0,0 +1,46 @@
+# Time:  O(n * logk) ~ O(n * k)
+# Space: O(n + k)
+
+# Write a program to find the nth super ugly number.
+#
+# Super ugly numbers are positive numbers whose all
+# prime factors are in the given prime list primes of size k.
+# For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32]
+# is the sequence of the first 12 super ugly numbers given
+# primes = [2, 7, 13, 19] of size 4.
+#
+# Note:
+# (1) 1 is a super ugly number for any given primes.
+# (2) The given numbers in primes are in ascending order.
+# (3) 0 < k <= 100, 0 < n <= 106, 0 < primes[i] < 1000.
+
+# Heap solution. (620ms)
+import heapq
+
+
+class Solution(object):
+    def nthSuperUglyNumber(self, n, primes):
+        """
+        :type n: int
+        :type primes: List[int]
+        :rtype: int
+        """
+        heap, uglies, idx, ugly_by_last_prime = [], [0] * n, [0] * len(primes), [0] * n
+        uglies[0] = 1
+
+        for k, p in enumerate(primes):
+            heapq.heappush(heap, (p, k))
+
+        for i in xrange(1, n):
+            uglies[i], k = heapq.heappop(heap)
+            ugly_by_last_prime[i] = k
+            idx[k] += 1
+            while ugly_by_last_prime[idx[k]] > k:
+                idx[k] += 1
+            heapq.heappush(heap, (primes[k] * uglies[idx[k]], k))
+
+        return uglies[-1]
+
+
+if __name__ == '__main__':
+    print Solution().nthSuperUglyNumber(12, [2, 3, 5, 7])
