ecb880d14fea3c6826bd3f9b38d1b1ac4628d009
xin wei <xin_wei@intuit.com>
2017-06-26 07:18:37 -0700

    wrong answer:
    Input:
    ["MedianFinder","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian"]
    [[],[-1],[],[-2],[],[-3],[],[-4],[],[-5],[]]
    Output:
    [null,null,-1,null,-1.50000,null,-1,null,1.00000,null,-1]
    Expected:
    [null,null,-1.00000,null,-1.50000,null,-2.00000,null,-2.50000,null,-3.00000]

--- learnpythonthehardway/find-median-from-data-stream.py
+++ learnpythonthehardway/find-median-from-data-stream.py
@@ -34,12 +34,20 @@ class MedianFinder:
         self.__min_heap = []
 
     def addNum(self, num):
-
-
-
+        if not self.__max_heap or num > -self.__max_heap[0]:
+            heappush(self.__min_heap, num)
+            if (len(self.__min_heap) > len(self.__max_heap) + 1):
+                heappush(self.__max_heap, -heappop(self.__min_heap))
+        else:
+            heappush(self.__max_heap, num)
+            if (len(self.__max_heap) > len(self.__min_heap) + 1):
+                heappush(self.__min_heap, -heappop(self.__max_heap))
 
     def findMedian(self):
         """
         Returns the median of current data stream
         :rtype: float
         """
+        return (-self.__max_heap[0] + self.__min_heap[0]) / 2.0 \
+            if len(self.__min_heap) == len(self.__max_heap) \
+            else self.__min_heap[0]
