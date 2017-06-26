a6718894de64d5306fdf15df5ab81a597856a7ca
xin wei <xin_wei@intuit.com>
2017-06-26 07:07:07 -0700

    accepted

--- learnpythonthehardway/rearrange-string-k-distance-apart.py
+++ learnpythonthehardway/rearrange-string-k-distance-apart.py
@@ -16,3 +16,23 @@ class Solution(object):
         cnts = defaultdict(int)
         for c in str:
             cnts[c] += 1
+
+        heap = []
+        for c, cnt in cnts.iteritems():
+            heappush(heap, [-cnt, c])
+
+        result = []
+        while heap:
+            used_cnt_chars = []
+            for _ in xrange(min(k, len(str) - len(result))):
+                if not heap:
+                    return ""
+                cnt_char = heappop(heap)
+                result.append((cnt_char[1]))
+                cnt_char[0] += 1
+                if cnt_char[0] < 0:
+                    used_cnt_chars.append(cnt_char)
+            for cnt_char in used_cnt_chars:
+                heappush(heap, cnt_char)
+
+        return "".join(result)
