29bd02be797296440aad128b7c4b4a23a9a38839
cbweixin <cbweixin@qq.com>
2017-06-26 22:58:40 -0700

    wrong answer Input: [2,null,1] Visualize Output: [2,null,1] Expected: [1,null,2]

---
+++ learnpythonthehardway/recover-binary-search-tree.py
@@ -0,0 +1,72 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Two elements of a binary search tree (BST) are swapped by mistake.
+#
+# Recover the tree without changing its structure.
+#
+# Note:
+# A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+#
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        if self:
+            serial = []
+            queue = [self]
+
+            while queue:
+                cur = queue[0]
+
+                if cur:
+                    serial.append(cur.val)
+                    queue.append(cur.left)
+                    queue.append(cur.right)
+                else:
+                    serial.append("#")
+
+                queue = queue[1:]
+
+            while serial[-1] == "#":
+                serial.pop()
+
+            return repr(serial)
+
+        else:
+            return None
+
+class Solution:
+    # @param root, a tree node
+    # @return a tree node
+    def recoverTree(self, root):
+        global pre, first, second
+        pre , first, second = None, None, None
+
+        def inorder(root):
+            global pre, first, second
+            if not root:
+                return
+            inorder(root.left)
+            if not pre:
+                pre = root
+            if pre.val > root.val:
+                if not first:
+                    first = pre
+                second = root
+
+        inorder(root)
+        if first and second:
+            first.val, second.val = second.val,first.val
+
+if __name__ == '__main__':
+    root = TreeNode(0)
+    root.left = TreeNode(1)
+    print root
+    print Solution().recoverTree(root)
+
