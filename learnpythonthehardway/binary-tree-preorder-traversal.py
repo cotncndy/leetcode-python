4e6a186db46f016cf32016bc89e7e3c8772aaca9
xin wei <xin_wei@intuit.com>
2017-06-27 15:55:24 -0700

    good, accepted.

---
+++ learnpythonthehardway/binary-tree-preorder-traversal.py
@@ -0,0 +1,39 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a binary tree, return the preorder traversal of its nodes' values.
+#
+# For example:
+# Given binary tree {1,#,2,3},
+#    1
+#     \
+#      2
+#     /
+#    3
+# return [1,2,3].
+#
+# Note: Recursive solution is trivial, could you do it iteratively?
+#
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        st, res = [], []
+        while st or root:
+            if root:
+                res.append(root.val)
+                if root.right:
+                    st.append(root.right)
+                root = root.left
+            else:
+                root = st.pop()
+        return res
