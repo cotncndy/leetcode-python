263f45ebcc1623c5702e7e121554b862a75a944b
xin wei <xin_wei@intuit.com>
2017-07-03 11:37:32 -0700

    accepted, 613 ms , 30%, vs java 29 ms.

--- learnpythonthehardway/number-of-islands-ii.py
+++ learnpythonthehardway/number-of-islands-ii.py
@@ -49,3 +49,4 @@ class Solution(object):
 
 if __name__ == '__main__':
     print Solution().numIslands2(3, 3, [[0, 1], [1, 2], [2, 1], [1, 0], [0, 2], [0, 0], [1, 1]])
+    print Solution().numIslands2(3, 3, [[0, 1], [0, 2], [2, 1], [1, 0], [0, 2], [0, 0], [1, 1]])
