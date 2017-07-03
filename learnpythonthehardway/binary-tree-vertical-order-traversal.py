2b8df4a2d3a2e373aca3bed092bbb77debcc9b12
xin wei <xin_wei@intuit.com>
2017-07-02 18:01:27 -0700

    accepted, 45 ms vs java 7 ms

---
+++ learnpythonthehardway/binary-tree-vertical-order-traversal.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import defaultdict
+# BFS + hash solution.
+class Solution(object):
+    def verticalOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        cols = defaultdict(list)
+        queue = [(root,0)]  # review how to initialize a queue
+        for node, i in queue:
+            if node :
+                cols[i].append(node.val)
+                queue += (node.left,i-1), (node.right,i+1) # review how to add multiple items in queue directly
+
+        return [cols[i] for i in xrange(min(cols.keys()), max(cols.keys())+1)] \
+                if cols else []
