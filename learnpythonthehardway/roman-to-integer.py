caf858a22742f3e0746e08c559f1d1afc3dc54c7
xin wei <xin_wei@intuit.com>
2017-07-04 16:29:25 -0700

    wrong answer:
    Your input
    
    "DCXXI"
    Your answer
    
    421
    Expected answer
    
    621

---
+++ learnpythonthehardway/roman-to-integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @return an integer
+    def romanToInt(self, s):
+        numeral_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        for i in xrange(len(s)):
+            if i == len(s) - 1 or s[i + 1] >= s[i]:
+                res += numeral_map[s[i]]
+            else:
+                res -= numeral_map[s[i]]
+
+        return res
