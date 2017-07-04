9e697611388afe2136acae6c70fbccf3aef48fe3
xin wei <xin_wei@intuit.com>
2017-07-04 11:03:34 -0700

    accepted, 156 ms, 89%

---
+++ learnpythonthehardway/longest-repeating-character-replacement.py
@@ -0,0 +1,50 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Given a string that consists of only uppercase English letters,
+# you can replace any letter in the string with another letter at most k times.
+# Find the length of a longest substring containing all repeating letters
+# you can get after performing the above operations.
+#
+# Note:
+# Both the string's length and k will not exceed 104.
+#
+# Example 1:
+#
+# Input:
+# s = "ABAB", k = 2
+#
+# Output:
+# 4
+#
+# Explanation:
+# Replace the two 'A's with two 'B's or vice versa.
+# Example 2:
+#
+# Input:
+# s = "AABABBA", k = 1
+#
+# Output:
+# 4
+#
+# Explanation:
+# Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+# The substring "BBBB" has the longest repeating letters, which is 4.
+
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        counts, res, start, max_char = [0] * 26, 0, 0, 0
+        for i, char in enumerate(s):
+            counts[ord(char) - ord('A')] += 1
+            max_char = max(max_char, counts[ord(char) - ord('A')])
+            while i + 1 - max_char - start > k:
+                counts[ord(s[start]) - ord('A')] -= 1
+                start += 1
+            res = max(res, i - start + 1)
+
+        return res
