7376ee3f064dffc88f670159575717c013a252fb
xin wei <xin_wei@intuit.com>
2017-07-03 17:18:38 -0700

    runtime error: Runtime Error Message: Line 15: IndexError: list index out of range Last executed input: "aa" 0

---
+++ learnpythonthehardway/longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,24 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        max_len, distint_count, start, visited = 0, 0, 0, [0 for _ in xrange(256)]
+        for i, char in enumerate(s):
+            if visited[ord(char)] == 0:
+                distint_count += 1
+            visited[ord(char)] += 1
+
+            while distint_count > k:
+                visited[start] -= 1
+                if visited[start] == 0:
+                    distint_count -= 1
+                start += 1
+
+            max_len = max(max_len, i - start + 1)
+        return max_len
