e9808340e856b52a4d3b3942cb2506edacd23f56
xin wei <xin_wei@intuit.com>
2017-07-04 16:50:51 -0700

    infinite loop

---
+++ learnpythonthehardway/divide-two-integers.py
@@ -0,0 +1,26 @@
+# Time:  O(logn) = O(1)
+# Space: O(1)
+#
+# Divide two integers without using multiplication, division and mod operator.
+
+
+class Solution:
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        sign = (dividend < 0) is (divisor < 0)  # review so concise
+        res, dividend, divisor = 0, abs(dividend), abs(divisor)
+
+        while dividend >= divisor:
+            t, m = divisor, 1
+            while dividend >= (t << 1):
+                t <<= 1
+                m <<= 1
+            res += m
+        if not sign:
+            res = -res
+
+        return min(max(-2147483648, res), 2147483647)
