4c37930fcbba0559212d88660bfc02c8c5bd6c42
xin wei <xin_wei@intuit.com>
2017-07-04 11:46:00 -0700

    accepted, 309ms, 45%

--- learnpythonthehardway/lru-cache.py
+++ learnpythonthehardway/lru-cache.py
@@ -82,6 +82,31 @@ class LRUCache(object):
         self.__insert(key, value)
 
 
+import collections
+
+
+class LRUCache2:
+    def __init__(self, capacity):
+        self.cache = collections.OrderedDict()  # review how orderedDict could be used as LRU cache
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        val = self.cache[key]
+        del self.cache[key]
+        self.cache[key] = val
+        return val
+
+    def set(self, key, value):
+        if key in self.cache:
+            del self.cache[key]
+        elif len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+
+
+
         # Your LRUCache object will be instantiated and called as such:
         # obj = LRUCache(capacity)
         # param_1 = obj.get(key)
