b860f321a64803d455c15bbbd3a4d64168634b1b
xin wei <xin_wei@intuit.com>
2017-07-03 20:17:47 -0700

    wrong answer:
    Input:
    "loveleetcode"
    Output:
    0
    Expected:
    2

--- learnpythonthehardway/first-unique-character-in-a-string.py
+++ learnpythonthehardway/first-unique-character-in-a-string.py
@@ -15,6 +15,8 @@
 import collections
 import string
 
+from collections import defaultdict
+
 
 class Solution(object):
     def firstUniqChar(self, s):
@@ -24,3 +26,20 @@ class Solution(object):
         """
         # review the usage of find(), that's really cool, like natural language.
         return min([s.find(c) for c in string.ascii_lowercase if s.count(c) == 1] or [-1])
+
+    def firstUniqChar2(self, s):
+        lookup = defaultdict(int)
+        candidates = set()
+
+        for i, char in enumerate(s):
+            if char in lookup:
+                candidates.discard(i)
+            else:
+                lookup[char] = i
+                candidates.add(i)
+
+        return min(candidates) if candidates else  -1
+
+
+if __name__ == '__main__':
+    print  Solution().firstUniqChar2("loveleetcode")
