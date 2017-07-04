c18dd93ee6790fb31b64175194cd6c5e3a077b02
xin wei <xin_wei@intuit.com>
2017-07-04 16:33:41 -0700

    accepted, 136ms, 87.71%

--- learnpythonthehardway/roman-to-integer.py
+++ learnpythonthehardway/roman-to-integer.py
@@ -4,9 +4,13 @@ class Solution:
         numeral_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         res = 0
         for i in xrange(len(s)):
-            if i == len(s) - 1 or s[i + 1] >= s[i]:
+            if i == len(s) - 1 or numeral_map[s[i + 1]] <= numeral_map[s[i]]:
                 res += numeral_map[s[i]]
             else:
                 res -= numeral_map[s[i]]
 
         return res
+
+
+if __name__ == '__main__':
+    print Solution().romanToInt("DCXXI")
