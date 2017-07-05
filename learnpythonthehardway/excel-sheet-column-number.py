6b0ff30271aa4d6860ff95cef6dda4306eda2cdb
xin wei <xin_wei@intuit.com>
2017-07-05 14:55:57 -0700

    accepted, 52ms, 53%

---
+++ learnpythonthehardway/excel-sheet-column-number.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Related to question Excel Sheet Column Title
+#
+# Given a column title as appear in an Excel sheet, return its corresponding column number.
+#
+# For example:
+#
+#     A -> 1
+#     B -> 2
+#     C -> 3
+#     ...
+#     Z -> 26
+#     AA -> 27
+#     AB -> 28
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        for i in xrange(len(s)):
+            res = res * 26 + ord(s[i]) - ord('A') + 1
+        return res
