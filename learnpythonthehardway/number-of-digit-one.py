d00ffbdf81d70d4ef374768158b9c2ef0171d745
xin wei <xin_wei@intuit.com>
2017-07-05 20:37:39 -0700

    accepted, 38ms, 97%

---
+++ learnpythonthehardway/number-of-digit-one.py
@@ -0,0 +1,18 @@
+# Time:  O(logn)
+# Space: O(1)
+#
+# Given an integer n, count the total number of digit 1 appearing
+# in all non-negative integers less than or equal to n.
+#
+# For example:
+# Given n = 13,
+# Return 6, because digit 1 occurred in the following numbers:
+#  1, 10, 11, 12, 13.
+#
+
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countDigitOne(self, n):
+        # todo  https://www.evernote.com/shard/s2/nl/20757047/267fab1f-ceb4-472d-8f84-497967b37488/
+        pass
