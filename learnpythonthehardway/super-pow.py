98e7f0458e070db9ef96cda375f53998496d99d2
xin wei <xin_wei@intuit.com>
2017-07-06 23:17:40 -0700

    wrong answer:
    Input:
    2
    [3]
    Output:
    1
    Expected:
    8

---
+++ learnpythonthehardway/super-pow.py
@@ -0,0 +1,39 @@
+# Time:  O(n), n is the size of b.
+# Space: O(1)
+
+# Your task is to calculate a^b mod 1337 where a is a positive integer
+# and b is an extremely large positive integer given in the form of an array.
+#
+# Example1:
+#
+# a = 2
+# b = [3]
+#
+# Result: 8
+# Example2:
+#
+# a = 2
+# b = [1,0]
+#
+# Result: 1024
+
+class Solution(object):
+    def superPow(self, a, b):
+        """
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        """
+
+        def pow(a, b):
+            if b == 1:
+                return a % 1337
+            if b == 0:
+                return 1
+            return ((pow(a, b / 2) % 1337) * (pow(a, b - b / 2) % 1337)) % 1337
+
+        res = 1
+        for i in xrange(len(b)):
+            res = ((pow(res, 10) % 1337) * (pow(res, b[i]) % 1337)) % 1337
+
+        return res
