16c2438cb8fdf3c85d082b74d445781b78348a06
xin wei <xin_wei@intuit.com>
2017-07-09 12:31:44 -0700

    wrong, the result whould be 2 but it is 1 for test case:
    [[0, 30],[5, 10],[15, 20]]

---
+++ learnpythonthehardway/meeting-rooms-ii.py
@@ -0,0 +1,31 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# Definition for an interval.
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+from collections import defaultdict
+
+
+class Solution:
+    # @param {Interval[]} intervals
+    # @return {integer}
+    def minMeetingRooms(self, intervals):
+        lookup = defaultdict(int)
+        for i in intervals:
+            lookup[i.start] += 1
+            lookup[i.end] -= 1
+
+        res, curr = 0, 0
+        for key, value in lookup.items():
+            res = max(res, curr + lookup[key])
+        return res
+
+
+if __name__ == '__main__':
+    intervals = [Interval(0, 30), Interval(5, 10), Interval(15, 20)]
+    print Solution().minMeetingRooms(intervals)
