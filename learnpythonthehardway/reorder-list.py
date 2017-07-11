7f3f2c567633bfc25a31b43cd455156f05cdc252
xin wei <xin_wei@intuit.com>
2017-07-11 10:20:28 -0700

    runtime error:
    Runtime Error Message:
    Line 20: ValueError: too many values to unpack
    Last executed input:
    [1,2]

---
+++ learnpythonthehardway/reorder-list.py
@@ -0,0 +1,47 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a singly linked list L: L0->L1->...->Ln-1->Ln,
+# reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2->...
+#
+# You must do this in-place without altering the nodes' values.
+#
+# For example,
+# Given {1,2,3,4}, reorder it to {1,4,2,3}.
+#
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, repr(self.next))
+
+
+class Solution:
+    # @param head, a ListNode
+    # @return nothing
+    def reorderList(self, head):
+        if head is None or head.next is None:
+            return head
+
+        fast, slow, prev = head, head, None
+        while fast and fast.next:
+            fast, slow, prev = fast.next.next, slow.next, slow
+
+        curr, prev.prev.next = prev, None, None
+        while curr is not None:  # review how to reverse a linkedlist by using just one sentence
+            curr.next, prev, curr = prev, curr, curr.next
+
+        l1, l2 = head, prev
+        dummy = ListNode(-1)
+        curr = dummy
+
+        while l1 is not None and l2 is not None:
+            curr.next, cur, l1 = l1, l1, l1.next
+            curr.next, cur, l2 = l2, l2, l2.next
+
+        return dummy.next
