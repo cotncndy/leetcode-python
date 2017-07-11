a7ad1c536cb854236d98ecbe5e5a1523aed97bc3
xin wei <xin_wei@intuit.com>
2017-07-11 11:25:26 -0700

    TLE:
    Last executed input:
    [0,0,0]
    0

---
+++ learnpythonthehardway/3sum-smaller.py
@@ -0,0 +1,22 @@
+# Time:  O(n^2)
+# Space: O(1)
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer}
+    def threeSumSmaller(self, nums, target):
+        nums.sort()
+        n = len(nums)
+
+        count, k = 0, 2
+        while k < n:
+            i, j = 0, k - 1
+            while i < j:
+                if nums[i] + nums[j] + nums[k] < target:
+                    count += j - i
+                    i += 1
+                else:
+                    j -= 1
+            k += 1
+        return count
