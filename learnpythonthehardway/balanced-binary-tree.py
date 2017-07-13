442ab57d3b6be50121322e3f285de4be8f0dfc66
xin wei <xin_wei@intuit.com>
2017-07-13 12:37:29 -0700

    accepted, 108ms, 23%

---
+++ learnpythonthehardway/balanced-binary-tree.py
@@ -0,0 +1,42 @@
+# Time:  O(n)
+# Space: O(h), h is height of binary tree
+#
+# Given a binary tree, determine if it is height-balanced.
+#
+# For this problem, a height-balanced binary tree is defined as a binary tree
+# in which the depth of the two subtrees of every node never differ by more than 1.
+#
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def isBalanced(self, root):
+        if not root:
+            return True
+        if abs(self.get_depth(root.left) - self.get_depth(root.right)) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def get_depth(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.get_depth(root.left), self.get_depth(root.right))
+
+
+if __name__ == "__main__":
+    root = TreeNode(0)
+    root.left = TreeNode(1)
+    result = Solution().isBalanced(root)
+    print result
+
+    root.left.left = TreeNode(2)
+    result = Solution().isBalanced(root)
+    print result
