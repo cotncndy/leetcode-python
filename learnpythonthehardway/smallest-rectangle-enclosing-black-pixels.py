1c4ee09fa1a152149c3f093c93857fc32bfe27b3
xin wei <xin_wei@intuit.com>
2017-07-16 15:01:02 -0700

    accepted, 78ms, 57%

--- learnpythonthehardway/smallest-rectangle-enclosing-black-pixels.py
+++ learnpythonthehardway/smallest-rectangle-enclosing-black-pixels.py
@@ -1,6 +1,6 @@
 # Time:  O(nlogn)
 # Space: O(1)
-
+import itertools
 class Solution(object):
     def minArea(self, image, x, y):
         """
@@ -16,3 +16,27 @@ class Solution(object):
                     left, right, up, down = min(left, i), max(right, i), min(up, j), max(down, j)
 
         return (right - left + 1) * (down - up + 1)
+
+    # review 1, function pointer 2.lamda
+    def minArea2(self, image, x, y):
+
+        def binary_search(left, right, find, imags,
+                          has_one):  # knowledge how python pass in function 'find' as parameter
+            while left <= right:
+                mid = (left + right) / 2
+                if find(image, has_one, mid):
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            return left
+
+        search_columns = lambda image, has_one, mid: any(int(row[mid]) for row in image) == has_one
+        left = binary_search(0, y - 1, search_columns, image, True)  # search the first 1
+        right = binary_search(y + 1, len(image[0]) - 1, search_columns, image, False)  # search the first 0, so False
+
+        search_rows = lambda image, has_one, mid: any(itertools.imap(int, image[mid])) == has_one
+        up = binary_search(0, x - 1, search_rows, image, True)
+        bottom = binary_search(x + 1, len(image) - 1, search_rows, image, False)
+
+        return (right - left) * (bottom - up)
