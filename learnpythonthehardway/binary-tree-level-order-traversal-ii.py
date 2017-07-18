f6cdf71ed7c60f772b76e4198e102ae4e37a3790
xin wei <xin_wei@intuit.com>
2017-07-18 07:51:33 -0700

    accepted, 92ms, 3%

---
+++ learnpythonthehardway/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,60 @@
+# Time:  O(n)
+# Space: O(n)
+
+# Given a binary tree, return the bottom-up level order traversal of its nodes' values.
+#  (ie, from left to right, level by level from leaf to root).
+#
+# For example:
+# Given binary tree {3,9,20,#,#,15,7},
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# return its bottom-up level order traversal as:
+# [
+#   [15,7],
+#   [9,20],
+#   [3]
+# ]
+
+# Definition for a  binary tree node
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        res, cur = [], [root]
+        while cur:
+            next_level, vals = [], []
+            for n in cur:
+                vals.append(n.val)
+                if n.left:
+                    next_level.append(n.left)
+                if n.right:
+                    next_level.append(n.right)
+
+            res.append(vals)
+            cur = next_level
+
+        return res[::-1]
+
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    result = Solution().levelOrderBottom(root)
+    print result
