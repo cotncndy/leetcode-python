57f5f5d41a55a25ae7167e68d842feafc587dda9
xin wei <xin_wei@intuit.com>
2017-07-22 15:17:46 -0700

    accepted.

--- learnpythonthehardway/count-univalue-subtrees.py
+++ learnpythonthehardway/count-univalue-subtrees.py
@@ -2,11 +2,16 @@
 # Space: O(h)
 #
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        if self:
+            return "{} : {},{}".format(self.val, self.left.val if self.left else 'None', \
+                                       self.right.val if self.right else 'None')
 
 class Solution:
     # @param {TreeNode} root
@@ -14,15 +19,27 @@ class Solution:
     count = 0
 
     def countUnivalSubtrees(self, root):
-        self.isUni(root, root.val)
+        self.isUni(root, -1)  # bugfixed
         return self.count
 
     def isUni(self, root, val):
         if not root:
             return True  # leaf is uni-tree
 
-        if not self.isUni(root.left, root.val) or not self.isUni(root.right, root.val):
+        if (not self.isUni(root.left, root.val)) | (
+        not self.isUni(root.right, root.val)):  # bugfixed, notice can not use or
+            # instead need to use '|'
             return False
 
         self.count += 1
         return root.val == val
+
+
+if __name__ == '__main__':
+    r = TreeNode(5)
+    r1, r2 = TreeNode(1), TreeNode(5)
+    r.left, r.right = r1, r2
+    r1.left, r1.right = TreeNode(5), TreeNode(5)
+    r2.left, r2.right = TreeNode(5), TreeNode(5)
+
+    print Solution().countUnivalSubtrees(r)
