89e8e614d948990779a08c99257f863f687bdc1a
xin wei <xin_wei@intuit.com>
2017-07-22 11:53:14 -0700

    accepted, 42ms, 58%

--- learnpythonthehardway/binary-tree-right-side-view.py
+++ learnpythonthehardway/binary-tree-right-side-view.py
@@ -25,4 +25,26 @@ class TreeNode:
 class Solution:
     # @param root, a tree node
     # @return a list of integers
+    # review dfs , really smart traversal
     def rightSideView(self, root):
+        res = []
+        self.dfs_traversal(root, 1, res)
+        return res
+
+    def dfs_traversal(self, root, depth, res):
+        if not root:
+            return res
+        if len(res) < depth:
+            res.append(root.val)
+        self.dfs_traversal(root.right, depth + 1, res)  # review firstly from right, then left
+        self.dfs_traversal(root.left, depth + 1, res)
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.right = TreeNode(5)
+    root.right.right = TreeNode(4)
+    result = Solution().rightSideView(root)
+    print result
