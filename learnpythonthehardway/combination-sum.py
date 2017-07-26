8783b015bc338cff8bc199c47d012b459587ee5d
xin wei <xin_wei@intuit.com>
2017-07-26 10:06:58 -0700

    acepted, 92ms, 80%

--- learnpythonthehardway/combination-sum.py
+++ learnpythonthehardway/combination-sum.py
@@ -22,21 +22,32 @@ class Solution:
     # @return a list of lists of integers
     def combinationSum(self, candidates, target):
         res = []
-        self.dfs(candidates, target, 0, [], res)
+        self.dfs(sorted(candidates), target, 0, [], res)  # fixed, need to sort them
+        # self.combinationSumRecu(sorted(candidates), res, 0, [], target)
 
         return res
 
     def dfs(self, candidates, target, start, cur, res):
         if target == 0:
             res.append(list(cur))
+            return
         while start < len(candidates) and candidates[start] <= target:  # bugfixed missing "="
             cur.append(candidates[start])
             self.dfs(candidates, target - candidates[start], start, cur, res)
             cur.pop()
             start += 1
 
+    def combinationSumRecu(self, candidates, result, start, intermediate, target):
+        if target == 0:
+            result.append(list(intermediate))
+        while start < len(candidates) and candidates[start] <= target:
+            intermediate.append(candidates[start])
+            self.combinationSumRecu(candidates, result, start, intermediate, target - candidates[start])
+            intermediate.pop()
+            start += 1
+
 
 if __name__ == "__main__":
-    candidates, target = [2, 3, 6, 7], 7
+    candidates, target = [8, 7, 4, 3], 11
     result = Solution().combinationSum(candidates, target)
     print result
