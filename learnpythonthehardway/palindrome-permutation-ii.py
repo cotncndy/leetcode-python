a3075c8d5dc23a866dabd2bf00f092355f240c7a
xin wei <xin_wei@intuit.com>
2017-07-29 16:25:32 -0700

    TLE:
    ast executed input:
    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"

--- learnpythonthehardway/palindrome-permutation-ii.py
+++ learnpythonthehardway/palindrome-permutation-ii.py
@@ -33,6 +33,35 @@ class Solution(object):
             st[i], st[start] = st[start], st[i]
             self.permute(''.join(st), start + 1, mid, res)
 
+    def generatePalindromes2(self, s):
+        cnt = collections.Counter(s)
+        mid = ''.join(k for k, v in cnt.iteritems() if v % 2)
+        if len(mid) > 1:
+            return []
+        chars = ''.join(k * (v / 2) for k, v in cnt.iteritems())
+
+        used, res = [False] * len(chars), []
+        self.permute2(chars, mid, used, [], res)
+
+        return res
+
+    def permute2(self, chars, mid, used, cur, res):
+        if len(cur) == len(chars):
+            half_palindrome = ''.join(cur)
+            res.append(half_palindrome + mid + half_palindrome[::-1])
+            return
+
+        for i in xrange(len(chars)):
+            if not used[i] and not (i > 0 and chars[i - 1] == chars[i] and used[i - 1]):
+                used[i] = True
+                cur.append(chars[i])
+                self.permute2(chars, mid, used, cur, res)
+                cur.pop()
+                used[i] = False
+
+
+
+
 
 if __name__ == '__main__':
-    print Solution().generatePalindromes('aaaabbbb')
+    print Solution().generatePalindromes2('aaaabbbb')
