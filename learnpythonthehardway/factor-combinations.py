8b70a7845ca8a6e60c48d3e136c613ad066c77c5
xin wei <xin_wei@intuit.com>
2017-07-29 14:54:36 -0700

    runtime error:
      File "/Users/xwei/Documents/bitbucket_repo/pythonStudy/learnpythonthehardway/factor-combinations.py", line 18, in backtrack
        res.append(list(factors))
    RuntimeError: maximum recursion depth exceeded while calling a Python object

---
+++ learnpythonthehardway/factor-combinations.py
@@ -0,0 +1,26 @@
+# Time:  O(nlogn)
+# Space: O(logn)
+
+class Solution:
+    # @param {integer} n
+    # @return {integer[][]}
+    def getFactors(self, n):
+        res, factors = [], []
+        self.backtrack(n, factors, res)
+        return res
+
+    def backtrack(self, n, factors, res):
+        i = 2 if not factors else factors[-1]
+        while i < n / i:
+            if n % i == 0:
+                factors.append(i)
+                factors.append(n / i)  # the i and n/i is a valid combination
+                res.append(list(factors))
+                factors.pop()  # pop out n / i, then prepare to go recursion
+                self.backtrack(n, factors, res)
+                factors.pop()  # pop again, eject i, prepare for the next loop
+            i += 1
+
+
+if __name__ == '__main__':
+    print Solution().getFactors(12)
