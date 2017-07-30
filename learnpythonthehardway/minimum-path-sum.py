bf857f6b92b75c76c163447ad9d0e48d51959fbb
xin wei <xin_wei@intuit.com>
2017-07-30 16:06:31 -0700

    accepted, 58ms, 70%

--- learnpythonthehardway/minimum-path-sum.py
+++ learnpythonthehardway/minimum-path-sum.py
@@ -13,11 +13,17 @@ class Solution:
     def minPathSum(self, grid):
         sum = list(grid[0])
         for i in xrange(1, len(grid[0])):
-            sum[i] += sum[i - 1] + grid[0][i]
+            sum[i] += sum[i - 1]  # bugfixed, since sum aleady is grid[0], no need to +grid[0][i]
+        print sum
 
         for i in xrange(1, len(grid)):
             sum[0] += grid[i][0]
             for j in xrange(1, len(grid[0])):
                 sum[j] = min(sum[j], sum[j - 1]) + grid[i][j]
+            print sum
 
         return sum[-1]
+
+
+if __name__ == '__main__':
+    print Solution().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]])
