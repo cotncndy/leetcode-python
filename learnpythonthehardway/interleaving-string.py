d3b59c392a2c4c156b78d207eb1c590dfab02ba8
xin wei <xin_wei@intuit.com>
2017-07-31 23:38:21 -0700

    run time error :
    Line 21: IndexError: string index out of range
    Last executed input:
    "aa"
    "ab"
    "aaba"

--- learnpythonthehardway/interleaving-string.py
+++ learnpythonthehardway/interleaving-string.py
@@ -35,3 +35,21 @@ class Solution:
                            or (match[i] and s2[j - 1] == s3[i + j - 1])
 
         return match[-1]
+
+    def isInterleave(self, s1, s2, s3):  # 2-d dp
+        if len(s1) + len(s2) != len(s3):
+            return False
+        match = [[False for i in xrange(len(s2) + 1)] for j in xrange(len(s1) + 1)]
+        match[0][0] = True
+        for i in xrange(1, len(s1) + 1):
+            match[i][0] = match[i - 1][0] and s1[i - 1] == s3[i - 1]
+
+        for j in xrange(1, len(s2) + 1):
+            match[0][j] = match[0][j - 1] and s2[j - 1] == s3[j - 1]
+
+        for i in xrange(1, len(s1) + 1):
+            for j in xrange(1, len(s2) + 1):
+                match[i][j] = (match[i - 1][j] and s1[i - 1][j] == s3[i + j - 1]) or \
+                              (match[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+
+        return match[-1][-1]
