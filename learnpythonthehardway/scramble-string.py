df5fca1988f78b5e63a36fd2f2cc6c2b4ff2f16c
xin wei <xin_wei@intuit.com>
2017-07-31 17:15:59 -0700

    accepted, method 3, 59ms, 91%

--- learnpythonthehardway/scramble-string.py
+++ learnpythonthehardway/scramble-string.py
@@ -42,9 +42,12 @@
 # DP solution
 # Time:  O(n^4)
 # Space: O(n^3)
+import collections
+
+
 class Solution:
     # @return a boolean
-    def isScramble(self, s1, s2):
+    def isScramble(self, s1, s2):  # review, 3-d dp , wonderful
         if not s1 or not s2 or len(s1) != len(s2):
             return False
         if s1 == s2:
@@ -65,3 +68,76 @@ class Solution:
                             dp[n][i][j] = True
 
         return dp[n][0][0]
+
+    def isScramble2(self, s1, s2, r=0):
+        if not s1 or s1 == s2: return True
+        dic = collections.Counter([])
+        different = 0
+        for i, c1, c2 in zip(range(len(s1)), s1, s2):
+            if c1 != c2:
+                dic[c1] += 1
+                dic[c2] -= 1
+                different += 2 - (dic[c1] <= 0) * 2 - (dic[c2] >= 0) * 2
+            if not different and i < len(s1) - 1:
+                if self.isScramble(s1[:i + 1], s2[:i + 1]) and self.isScramble(s1[i + 1:], s2[i + 1:]): return True
+        if not r and not different and self.isScramble(s1, s2[::-1], 1): return True
+        return False
+
+    def isScramble3(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) != len(s2):
+            return False
+
+        memo = {}
+        return self.search(s1, s2, memo)
+
+    def search(self, s1, s2, memo):  # review top-down dp, so cool
+        if memo.get(s1 + '#' + s2) != None:
+            return memo[s1 + '#' + s2]
+
+        n = len(s1)
+        if n == 1:
+            return s1 == s2
+        elif not sorted(s1) == sorted(s2):
+            return False
+        for i in range(1, n):
+            if self.search(s1[:i], s2[:i], memo) and self.search(s1[i:], s2[i:], memo) or \
+                            self.search(s1[:i], s2[n - i:], memo) and self.search(s1[i:], s2[:n - i], memo):
+                memo[s1 + '#' + s2] = True
+                return True
+        memo[s1 + '#' + s2] = False
+        return False
+
+    def isScramble4(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+
+        def find(w1, w2):
+            if w1 == w2:
+                return True
+            m = {}
+            for w in w1:
+                if w not in m:
+                    m[w] = 1
+                else:
+                    m[w] += 1
+            for w in w2:
+                if w not in m or m[w] == 0:
+                    return False
+                m[w] -= 1
+            l = len(w1)
+            for i in xrange(1, l):
+                if find(w1[:i], w2[:i]) and find(w1[i:], w2[i:]):
+                    return True
+                elif find(w1[:i], w2[l - i:]) and find(w1[i:], w2[:l - i]):
+                    return True
+            return False
+
+        return find(s1, s2)
