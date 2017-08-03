ce482aaee329983a1ff75d9bd45607268c7e24f1
xin wei <xin_wei@intuit.com>
2017-08-03 16:41:35 -0700

    accepted, 42ms, 63%

--- learnpythonthehardway/paint-house.py
+++ learnpythonthehardway/paint-house.py
@@ -12,9 +12,14 @@ class Solution(object):
         n = len(costs)
         if not n:
             return 0
-        min_costs = [costs[0], [0, 0, 0] * (n - 1)]
+        min_costs = [costs[0]]
         for i in xrange(1, n):
+            min_costs.append([0, 0, 0])  # bugfixed
             for j in xrange(0, 3):
                 min_costs[i][j] = min(min_costs[i - 1][(j + 1) % 3], min_costs[i - 1][(j + 2) % 3]) + costs[i][j]
 
         return min(min_costs[-1][0], min_costs[-1][1], min_costs[-1][2])
+
+
+if __name__ == '__main__':
+    print Solution().minCost([[7, 6, 2]])
