3c4f6bfd4f34a3d1b7f1c7d3caf9cd0fd82e09a6
xin wei <xin_wei@intuit.com>
2017-08-07 20:38:11 -0700

    need to take a look why this method is that fast?

--- learnpythonthehardway/bomb-enemy.py
+++ learnpythonthehardway/bomb-enemy.py
@@ -68,6 +68,22 @@ class Solution(object):
 
         return res
 
+    def maxKilledEnemies3(self, grid):  # todo , take a look
+        def hits(grid):
+            return [
+                [h
+                 for block in ''.join(row).split('W')
+                 for h in [block.count('E')] * len(block) + [0]
+                 ]
+                for row in grid]
+
+        rowhits = hits(grid)
+        colhits = zip(*hits(zip(*grid)))
+        return max([rh + ch
+                    for row in zip(grid, rowhits, colhits)
+                    for cell, rh, ch in zip(*row)
+                    if cell == '0'] or [0])
+
 
 
 
