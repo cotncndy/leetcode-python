df4dcbcfe6f35863441c8872942cc8111b8fe75c
xin wei <xin_wei@intuit.com>
2017-08-07 19:36:25 -0700

    check in

---
+++ learnpythonthehardway/bomb-enemy.py
@@ -0,0 +1,9 @@
+# Time:  O(m * n)
+# Space: O(m * n)
+
+class Solution(object):
+    def maxKilledEnemies(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
