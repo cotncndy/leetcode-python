0d36ed0e97729b6313ecc766098a6348acc22e4b
xin wei <xin_wei@intuit.com>
2017-08-11 13:43:52 -0700

    accepted, 52ms, 22%

--- learnpythonthehardway/best-time-to-buy-and-sell-stock-ii.py
+++ learnpythonthehardway/best-time-to-buy-and-sell-stock-ii.py
@@ -19,3 +19,6 @@ class Solution:
         for i in xrange(len(prices) - 1):
             profit += max(0, prices[i + 1] - prices[i])
         return profit
+
+    def maxProfit2(self, prices):
+        return sum(map(lambda x: max(prices[x + 1] - prices[x], 0), range(len(prices[:-1]))))
