10a5f440b7a85ae27b306914feba08b7c80b6493
xin wei <xin_wei@intuit.com>
2017-08-11 12:49:25 -0700

    runtime error:
    "aa"
    "a"
    Your answer
    
    Line 10: IndexError: string index out of range
    Expected answer
    
    false

---
+++ learnpythonthehardway/wildcard-matching.py
@@ -0,0 +1,45 @@
+# Time:  O(m + n)
+# Space: O(1)
+#
+# Implement wildcard pattern matching with support for '?' and '*'.
+#
+# '?' Matches any single character.
+# '*' Matches any sequence of characters (including the empty sequence).
+#
+# The matching should cover the entire input string (not partial).
+#
+# The function prototype should be:
+# bool isMatch(const char *s, const char *p)
+#
+# Some examples:
+# isMatch("aa","a") -> false
+# isMatch("aa","aa") -> true
+# isMatch("aaa","aa") -> false
+# isMatch("aa", "*") -> true
+# isMatch("aa", "a*") -> true
+# isMatch("ab", "?*") -> true
+# isMatch("aab", "c*a*b") -> false
+#
+
+# iteration
+class Solution:
+    # @param s, an input string
+    # @param p, a pattern string
+    # @return a boolean
+    def isMatch(self, s, p):
+        scur, pcur, sstar, pstar = 0, 0, -1, -1
+        while scur < len(s):
+            if s[scur] == p[pcur] or p[pcur] == '?':
+                scur, pcur = scur + 1, pcur + 1
+            elif pcur < len(p) and p[pcur] == '*':
+                pstar, pcur, sstar = pcur, pcur + 1, scur
+            elif pstar > -1:
+                pcur, scur = pstar, sstar
+                sstar += 1
+            else:
+                return False
+
+        while p[pcur] == '*':
+            pcur += 1
+
+        return pcur == len(p)
