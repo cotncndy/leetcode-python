cf1c4725369a41909977947fe53b8fc4c9044de8
xin wei <xin_wei@intuit.com>
2017-08-11 15:56:40 -0700

    accepted, 52ms, 77%, finally succeded.

--- learnpythonthehardway/remove-duplicate-letters.py
+++ learnpythonthehardway/remove-duplicate-letters.py
@@ -29,15 +29,19 @@ class Solution(object):
 
         res = []
         for c in s:
-            if len(res) == 0 or c not in res and (map[res[-1]] == 0 or c > res[-1]):  # bugfixed
+            if not res or (c not in res and (map[res[-1]] == 0 or c > res[-1])):  # bugfixed
                 res += c
-            elif c < res[-1] and map[
-                res[-1]] > 0:  # this should be  a while loop to remove any letter is bigger than cur˚
-                res[-1] = c
+            elif c not in res:  # bugfixed
+                while res and c < res[-1] and map[
+                    res[-1]] > 0:  # this should be  a while loop to remove any letter is bigger than cur
+                    res = res[:-1]
+                res += c
             map[c] -= 1
 
         return "".join(res)
 
 
 if __name__ == '__main__':
-    print Solution().removeDuplicateLetters('cbacdcbc')
+    print Solution().removeDuplicateLetters('cbabc')
+    print Solution().removeDuplicateLetters("cbacdcbc")
+    print Solution().removeDuplicateLetters("bbcaac")
