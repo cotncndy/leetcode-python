1b6dc9423610eda0d51bc1b6b25751651785ef33
xin wei <xin_wei@intuit.com>
2017-08-14 16:11:36 -0700

    wrong answer:
    []
    Output:
    1
    Expected:
    0

--- learnpythonthehardway/wiggle-subsequence.py
+++ learnpythonthehardway/wiggle-subsequence.py
@@ -53,3 +53,13 @@ class Solution(object):
                     neg[i] = max(neg[i], pos[j] + 1)
 
         return max(pos[-1], neg[-1])
+
+    def wiggleMaxLength2(self, nums):
+        pos, neg = 1, 1
+        for i in xrange(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                pos = neg + 1
+            elif nums[i] < nums[i - 1]:
+                neg = pos + 1
+
+        return max(pos, neg)
