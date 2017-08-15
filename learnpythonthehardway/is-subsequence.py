9d1c7148cefbd96354443740a1cff4e15ffc8c67
xin wei <xin_wei@intuit.com>
2017-08-14 19:31:12 -0700

    accepted, 258ms, 58%

--- learnpythonthehardway/is-subsequence.py
+++ learnpythonthehardway/is-subsequence.py
@@ -37,3 +37,20 @@ class Solution(object):
                 j += 1
 
         return i == len(s)
+
+    def isSubsequence2(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if not s:
+            return True
+        i = 0
+        for c in t:
+            if c == s[i]:
+                i += 1
+            if i == len(s):
+                break
+
+        return i == len(s)
