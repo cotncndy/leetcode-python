66c174d6a07188df13f79bdc5b2a5e60c55648bd
xin wei <xin_wei@intuit.com>
2017-08-18 16:50:38 -0700

    copy some other solutions which is faster than mine.

--- learnpythonthehardway/word-abbreviation.py
+++ learnpythonthehardway/word-abbreviation.py
@@ -16,6 +16,8 @@
 # The length of each word is greater than 1.
 # The words consist of lowercase English letters only.
 # The return answers should be in the same order as the original array.
+import collections
+
 
 class Solution(object):
     def wordsAbbreviation(self, dict):
@@ -47,7 +49,126 @@ class Solution(object):
     def abbrev(self, s, k):
         return s if k >= len(s) - 2 else s[0:k] + str(len(s) - k - 1) + s[-1]
 
+    def wordsAbbreviation2(self, ds):
+        """
+        :type dict: List[str]
+        :rtype: List[str]
+        """
+        return self.wa2(ds)
+
+    def wa1(self, ds):
+
+        ns = {}
+        for s in ds:
+            n = len(s)
+            ns[n] = ns.get(n, [])
+            ns[n].append(s)
+        for n in ns:
+            ns[n].sort(key=lambda s: s[0] + s[-1] + s[1:-1])
+
+        def diff(s, t):
+            i = 0
+            for i in xrange(len(s)):
+                if s[i] != t[i]:
+                    break
+            return i + 1
+
+        ss = {}
+        for n in ns:
+            for i, s in enumerate(ns[n]):
+                ret = 1
+                for j in xrange(i - 1, -1, -1):
+                    t = ns[n][j]
+                    if s[0] != t[0] or s[-1] != t[-1]:
+                        break
+                    ret = max(ret, diff(s, t))
+                for j in xrange(i + 1, len(ns[n])):
+                    t = ns[n][j]
+                    if s[0] != t[0] or s[-1] != t[-1]:
+                        break
+                    ret = max(ret, diff(s, t))
+                ss[s] = ret
+
+        def pshort(s):
+            i = ss[s]
+            zs = str(len(s) - 1 - i)
+            size = i + len(zs) + 1
+            return s[:i] + zs + s[-1] if size < len(s) else s
+
+        return [pshort(s) for s in ds]
+
+    def wa2(self, ds):
+
+        ns = {}
+        for s in ds:
+            key = (len(s), s[0], s[-1])  # knowledge how to write a compound key in python
+            ns[key] = ns.get(key, [])  # knowledge for hashmap, get(key, optional] , like java getOrDefault()
+            ns[key].append(s)
+        for key in ns:
+            ns[key].sort()
+
+        def diff(s, t):
+            i = 0
+            for i in xrange(len(s)):
+                if s[i] != t[i]:
+                    break
+            return i + 1
+
+        ss = {}
+        for n in ns:
+            for i, s in enumerate(ns[n]):
+                ret = 1
+                for j, t in enumerate(ns[n]):
+                    if i == j: continue
+                    ret = max(ret, diff(s, t))
+                ss[s] = ret
+
+        def pshort(s):
+            i = ss[s]
+            zs = str(len(s) - 1 - i)
+            size = i + len(zs) + 1
+            return s[:i] + zs + s[-1] if size < len(s) else s
+
+        return [pshort(s) for s in ds]
+
+    def wordsAbbreviation3(self, A):
+        """
+        :type dict: List[str]
+        :rtype: List[str]
+        """
+
+        def longest_common_prefix(a, b):
+            i = 0
+            while i < len(a) and i < len(b) and a[i] == b[i]:
+                i += 1
+            return i
+
+        ans = [None for _ in A]
+        # create a hash table, with key as (len(word), word[0]), word[-1]) and value as (word, index)
+        groups = collections.defaultdict(list)
+        for index, word in enumerate(A):
+            groups[len(word), word[0], word[-1]].append((word, index))
+        # iterate key and values, sort the value array first, longest common prefix start with 0
+        for (size, first, last), enum_words in groups.iteritems():
+            enum_words.sort()
+            lcp = [0] * len(enum_words)
+            for i, (word, _) in enumerate(enum_words):
+                if i:
+                    word2, _ = enum_words[i - 1]
+                    p = longest_common_prefix(word, word2)
+                    lcp[i] = max(lcp[i], p)
+                    lcp[i - 1] = max(lcp[i - 1], p)
+                    # zip word, and lcp together, create answer.
+            for (word, index), p in zip(enum_words, lcp):
+                delta = size - 2 - p
+                if delta <= max(1, len(str(delta)) - 1):
+                    ans[index] = word
+                else:
+                    ans[index] = word[:p + 1] + str(delta) + last
+
+        return ans
+
 
 if __name__ == '__main__':
     dict = ["like", "god", "internal", "me", "internet", "interval", "intension", "face", "intrusion"]
-    print Solution().wordsAbbreviation(dict)
+    print Solution().wordsAbbreviation2(dict)
