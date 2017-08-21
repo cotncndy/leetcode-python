8a184831492c6092516fd7d67521c5ef3c80e02c
xin wei <xin_wei@intuit.com>
2017-08-21 12:30:56 -0700

    File "/Users/xwei/Documents/bitbucket_repo/pythonStudy/learnpythonthehardway/longest-line-of-consecutive-1-in-matrix.py", line 27, in longestLine     dp[i][j][k] = 1 TypeError: 'int' object does not support item assignment

---
+++ learnpythonthehardway/longest-line-of-consecutive-1-in-matrix.py
@@ -0,0 +1,42 @@
+# Given a 01 matrix M, find the longest line of consecutive one in the matrix. The line could be horizontal,
+# vertical, diagonal or anti-diagonal.
+#
+# Example:
+# Input:
+# [[0,1,1,0],
+#  [0,1,1,0],
+#  [0,0,0,1]]
+# Output: 3
+# Hint: The number of elements in the given matrix will not exceed 10,000.
+
+class Solution(object):
+    def longestLine(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        if not len(M) or not len(M[0]):
+            return 0
+        row, col, res = len(M), len(M[0]), 0
+        dp = [[0] * 4 for _ in xrange(col) for _ in xrange(row)]
+        for i in xrange(row):
+            for j in xrange(col):
+                if M[i][j] == 0:
+                    continue
+                for k in xrange(4):
+                    dp[i][j][k] = 1
+                if j:
+                    dp[i][j][k] += dp[i][j - 1][k]  # horizontally
+                if i:
+                    dp[i][j][k] += dp[i - 1][j][k]  # vertically
+                if i and j:
+                    dp[i][j][k] += dp[i - 1][j - 1][k]  # diag
+                if i and j < col - 1:
+                    dp[i][j][k] += dp[i - 1][j + 1][k]  # anti-diag
+                res = max(res, dp[i][j][k])
+
+        return res
+
+
+if __name__ == '__main__':
+    print Solution().longestLine([[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 1]])
