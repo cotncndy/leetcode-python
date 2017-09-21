fadfb57e9e94b111643ea68632315ecf62c1de97
xin wei <xin_wei@intuit.com>
2017-09-20 22:37:18 -0700

    this solution can only got the positive one, I don't know how to use it to calculate the negative one.

---
+++ learnpythonthehardway/convet-number-to-hex.py
@@ -0,0 +1,43 @@
+# used.
+#
+# Note:
+#
+# All letters in hexadecimal (a-f) must be in lowercase.
+# The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero
+# character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+# The given number is guaranteed to fit within the range of a 32-bit signed integer.
+# You must not use any method provided by the library which converts/formats the number to hex directly.
+# Example 1:
+#
+# Input:
+# 26
+#
+# Output:
+# "1a"
+# Example 2:
+#
+# Input:
+# -1
+#
+# Output:
+# "ffffffff"
+
+
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+        res = []
+        while num > 16:
+            res.append(nums[num % 16])
+            num /= 16
+        res.append(nums[num])
+
+        return ''.join(res[::-1])
+
+
+if __name__ == '__main__':
+    print Solution().toHex(350)
