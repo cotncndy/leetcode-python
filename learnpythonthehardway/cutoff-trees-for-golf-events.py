39a01a11bbffcea7c3f78ec410320dd58a7520d5
xin wei <xin_wei@intuit.com>
2017-09-27 06:03:52 -0700

    another TLE algorithm from leetcode, but it is a lot more concise.

--- learnpythonthehardway/cutoff-trees-for-golf-events.py
+++ learnpythonthehardway/cutoff-trees-for-golf-events.py
@@ -96,6 +96,53 @@ class Solution(object):
         return -1
 
 
+class Solution2(object):
+    def cutOffTree(self, G):
+        """
+        :type forest: List[List[int]]
+        :rtype: int
+        """
+        if not G or not G[0]: return -1
+        m, n = len(G), len(G[0])
+        trees = []
+        for i in xrange(m):
+            for j in xrange(n):
+                if G[i][j] > 1:
+                    trees.append((G[i][j], i, j))  # add a tuple
+        trees = sorted(trees)
+        count = 0
+        cx, cy = 0, 0
+        for h, x, y in trees:
+            step = self.BFS(G, cx, cy, x, y)
+            if step == -1:
+                return -1
+            else:
+                count += step
+                G[x][y] = 1
+                cx, cy = x, y
+        return count
+
+    def BFS(self, G, cx, cy, tx, ty):
+        m, n = len(G), len(G[0])
+        visited = [[False for j in xrange(n)] for i in xrange(m)]
+        Q = collections.deque()
+        step = -1
+        Q.append((cx, cy))
+        while len(Q) > 0:
+            size = len(Q)
+            step += 1
+            for i in xrange(size):
+                x, y = Q.popleft()
+                visited[x][y] = True
+                if x == tx and y == ty:
+                    return step
+                for nx, ny in [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]:
+                    if nx < 0 or nx >= m or ny < 0 or ny >= n or G[nx][ny] == 0 or visited[nx][ny]:
+                        continue
+                    Q.append((nx, ny))
+        return -1
+
+
 if __name__ == '__main__':
     print Solution().cutOffTree([[1, 2, 3], [0, 0, 4], [7, 6, 5]])
     print Solution().cutOffTree([[1, 2, 3], [0, 0, 0], [7, 6, 5]])
