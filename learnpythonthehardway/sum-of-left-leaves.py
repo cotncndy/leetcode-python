14e3bde617f44b8e64a34df9dd56a74da78f8f34
xin wei <xin_wei@intuit.com>
2017-10-01 07:01:52 -0700

    another good solution

--- learnpythonthehardway/sum-of-left-leaves.py
+++ learnpythonthehardway/sum-of-left-leaves.py
@@ -37,3 +37,13 @@ class Solution(object):
             return helper(root.left, True) + helper(root.right, False)
 
         return helper(root, False)  # bugfixed, forget to pass in parameter
+
+    def sumOfLeftLeaves2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
