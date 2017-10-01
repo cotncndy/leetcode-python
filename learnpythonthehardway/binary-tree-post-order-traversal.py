3d475d68e8518e09cb21c4d46f222192b3f4abe5
xin wei <xin_wei@intuit.com>
2017-10-01 05:50:39 -0700

    run time error
    []
    Your answer
    
    Line 17: AttributeError: 'NoneType' object has no attribute 'left'
    Expected answer
    
    []

---
+++ learnpythonthehardway/binary-tree-post-order-traversal.py
@@ -0,0 +1,37 @@
+# Given a binary tree, return the postorder traversal of its nodes' values.
+#
+# For example:
+# Given binary tree {1,#,2,3},
+#    1
+#     \
+#      2
+#     /
+#    3
+# return [3,2,1].
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        st, prev, res = [root], root, []
+        while st:
+            top = st[-1]
+            if (top.left is None and top.right is None) or top.left == prev or top.right == prev:
+                res.apppend(top)
+                prev = top
+                res.pop()
+            elif top.right is not None:
+                res.append(top.right)
+            elif top.left is not None:
+                res.append(top.left)
+
+        return res
