236d9c2b199d0a20aeb40a3c6fa6d3ecbdc51cf1
xin wei <xin_wei@intuit.com>
2017-10-09 13:12:28 -0700

    run time error:
    Run Code Result:×
    
    Your input
    
    [[1,2],[3],[4,5,6]]
    Your answer
    
    Line 18: IndexError: list index out of range
    Expected answer
    
    [1,2,3,4,5,6]

---
+++ learnpythonthehardway/flattern-2d-vector.py
@@ -0,0 +1,47 @@
+# Implement an iterator to flatten a 2d vector.
+#
+# For example,
+# Given 2d vector =
+#
+# [
+#   [1,2],
+#   [3],
+#   [4,5,6]
+# ]
+# By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,
+# 5,6].
+#
+# Follow up:
+# As an added challenge, try to code it using only iterators in C++ or iterators in Java.
+
+
+class Vector2D(object):
+    def __init__(self, vec2d):
+        """
+        Initialize your data structure here.
+        :type vec2d: List[List[int]]
+        """
+        self.v = vec2d
+        self._row = 0
+        self._col = 0
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        self._col += 1
+        return self.v[self._row][self._col]
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if self._col == len(self.v[self._row]):
+            self._row, self._col = self._row + 1, 0
+
+        return self._row < len(self.v)
+
+
+        # Your Vector2D object will be instantiated and called as such:
+        # i, v = Vector2D(vec2d), []
+        # while i.hasNext(): v.append(i.next())
