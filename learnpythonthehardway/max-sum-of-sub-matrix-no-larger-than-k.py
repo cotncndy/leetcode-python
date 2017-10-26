6008dbe7dac2298ad1d5248103e23eb1b32538b6
xin wei <xin_wei@intuit.com>
2017-10-26 12:53:22 -0700

    wrong answer: for [[2,2,1]] , it should return 1,but it return -inf

--- learnpythonthehardway/max-sum-of-sub-matrix-no-larger-than-k.py
+++ learnpythonthehardway/max-sum-of-sub-matrix-no-larger-than-k.py
@@ -49,4 +49,44 @@ class Solution(object):
         :rtype: int
         """
         # todo
-        pass
+
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+        row, col, res = len(matrix), len(matrix[0]), float('-inf')
+        dp = [[0] * col for _ in xrange(row)]
+        for i in xrange(row):
+            for j in xrange(col):
+                t = matrix[i][j]
+                if i == 0 and j > 0:
+                    t += dp[i][j - 1]
+                elif j == 0 and i > 0:
+                    t += dp[i - 1][j]
+                elif i > 0 and j > 0:
+                    t += dp[i - 1][j - 1]
+                dp[i][j] = t
+
+                for r in xrange(i, row):
+                    for s in xrange(j, row):
+                        t = dp[r][s]
+                        if j > 0:
+                            t -= dp[r][j - 1]
+                            if i == 0:
+                                if t <= k:
+                                    res = max(res, t)
+                        if i > 0:
+                            t -= dp[i - 1][s]
+                            if j == 0:
+                                if t <= k:
+                                    res = max(res, t)
+                        if i > 0 and j > 0:
+                            t += dp[i - 1][j - 1]
+                            if t <= k:
+                                res = max(res, t)
+                        else:
+                            if t <= k:
+                                res = max(res, t)
+        return res
+
+
+if __name__ == '__main__':
+    print Solution().maxSumSubmatrix([[2, 2, 1]], 1)
