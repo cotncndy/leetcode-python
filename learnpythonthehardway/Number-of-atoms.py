9ccab19c6f439f40bbbbf306a20aca59183d66fa
xin wei <xin_wei@intuit.com>
2017-11-16 11:52:43 -0800

    passed.

--- learnpythonthehardway/Number-of-atoms.py
+++ learnpythonthehardway/Number-of-atoms.py
@@ -7,20 +7,46 @@ class Solution(object):
         :type formula: str
         :rtype: str
         """
-        map, ele, level = collections.orderedDict(), "", 0
 
-        for i in xrange(len(formula)):
-            if formula[i].isdigit():
-                map[ele], ele = (level, map[ele][1] * (ord(formula[i]) - ord('0'))), ""
-            elif formula[i].islower():
-                ele.append(formula[i])
-            elif formula[i].isupper():
-                if ele:
-                    map[ele], ele = (level, 1), ""
+        def parse():  # return collections.Counter
+            counter = collections.Counter()
+            while self.i < len(formula) and formula[self.i] != ')':
+                if formula[self.i] == '(':
+                    self.i += 1
+                    for name, v in parse().items():  # knowledge how to loop collections.counter
+                        counter[name] += v
                 else:
-                    ele.append(formula[i])
-            elif formula[i] == '(':
-                if not formula[i].isdigit():
-                    map[ele] = 1
-                ele = ""
-                stack.append(formula[i])
+                    i_start = self.i  # i_start must be uppercase  because the code below bypassed all lowercase and
+                    # digital
+                    self.i += 1
+                    while self.i < len(formula) and formula[self.i].islower():  # search all lowercase
+                        self.i += 1
+                    temp = formula[i_start:self.i]
+                    i_start = self.i
+                    while self.i < len(formula) and formula[self.i].isdigit():  # search all digital
+                        self.i += 1
+                    counter[temp] += int(formula[i_start:self.i] or 1)  # knowledge , a concise way
+
+            self.i += 1  # bypass the ')', go to the next one
+            i_start = self.i
+            while self.i < len(formula) and formula[self.i].isdigit():  # search all digital
+                self.i += 1
+            times = int(formula[i_start:self.i] or 1)
+            for name, v in counter.items():
+                counter[name] = v * times
+            return counter
+
+        self.i, ans = 0, []
+        count = parse()
+        for name in sorted(count):  # knowledge how to sort items
+            ans.append(name)
+            multiplicity = count[name]
+            if multiplicity > 1:
+                ans.append(str(multiplicity))
+        return "".join(ans)
+
+
+if __name__ == '__main__':
+    print Solution().countOfAtoms("Mg(OH)2")
+    print Solution().countOfAtoms("H2O")
+    print Solution().countOfAtoms("K4(ON(SO3)2)2")
