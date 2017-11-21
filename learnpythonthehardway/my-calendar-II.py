267eac89b1e33f0f37392dbd4e08b7393dafb997
xin wei <xin_wei@intuit.com>
2017-11-21 12:29:00 -0800

    MyCalendarII is using same algorithm as book3, only differences is that the check of intersection is put outside into another method, but MyCalendarII inline the check. MyCalendarII passed the OJ with runtime of 526ms. 我他妈能说什么。

--- learnpythonthehardway/my-calendar-II.py
+++ learnpythonthehardway/my-calendar-II.py
@@ -105,6 +105,22 @@ class MyCalendarTwo(object):
         return True
 
 
+class MyCalendarII(object):
+    def __init__(self):
+        self.overlaps = []
+        self.calendar = []
+
+    def book(self, start, end):
+        for i, j in self.overlaps:
+            if start < j and end > i:
+                return False
+        for i, j in self.calendar:
+            if start < j and end > i:
+                self.overlaps.append((max(start, i), min(end, j)))
+        self.calendar.append((start, end))
+        return True
+
+
 
 
 
