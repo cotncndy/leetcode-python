d744b3869b215f71209546d28556a8238155976d
xin wei <xin_wei@intuit.com>
2018-01-30 06:21:28 -0800

    Runtime Error × Run Code Result: Your input [2,1,3] Your answer Line 24: TypeError: cannot concatenate 'str' and 'int' objects Expected answer [2,1,3]

---
+++ learnpythonthehardway/serialize-deserialize-binary-search-tree.py
@@ -0,0 +1,79 @@
+# Serialization is the process of converting a data structure or object into a sequence of bits so that it can be
+# stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the
+#  same or another computer environment.
+#
+# Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your
+# serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be
+# serialized to a string and this string can be deserialized to the original tree structure.
+#
+# The encoded string should be as compact as possible.
+#
+# Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms
+# should be stateless.
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+import collections
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        vals, res = collections.deque(), ""
+        if not root:
+            return res
+        vals.append(root)
+
+        while vals:
+            t = vals.popleft()
+            if t:
+                res += t.val
+                if root.left:
+                    vals.append(root.left)
+                if root.right:
+                    vals.append(root.right)
+            else:
+                res += '#'
+
+        return res
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = collections.deque(val for val in data)
+        root = TreeNode(int(vals.popleft()))
+        cur, que = root, collections.deque()
+        que.append(cur)
+
+        while que:
+            cur = que.popleft()
+            t = vals.popleft()
+            if t != '#':
+                node = TreeNode(int(t))
+                cur.left = node
+                que.append(node)
+            t = vals.popleft()
+            if t != '#':
+                node = TreeNode(int(t))
+                cur.right = node
+                que.append(node)
+
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
---
+++ learnpythonthehardway/string-compression.py
@@ -0,0 +1,78 @@
+# Given an array of characters, compress it in-place.
+#
+# The length after compression must always be smaller than or equal to the original array.
+#
+# Every element of the array should be a character (not int) of length 1.
+#
+# After you are done modifying the input array in-place, return the new length of the array.
+#
+#
+# Follow up:
+# Could you solve it using only O(1) extra space?
+#
+#
+# Example 1:
+# Input:
+# ["a","a","b","b","c","c","c"]
+#
+# Output:
+# Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+#
+# Explanation:
+# "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+# Example 2:
+# Input:
+# ["a"]
+#
+# Output:
+# Return 1, and the first 1 characters of the input array should be: ["a"]
+#
+# Explanation:
+# Nothing is replaced.
+# Example 3:
+# Input:
+# ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+#
+# Output:
+# Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+#
+# Explanation:
+# Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb" is replaced by "b12".
+# Notice each digit has it's own entry in the array.
+# Note:
+# All characters have an ASCII value in [35, 126].
+# 1 <= len(chars) <= 1000.
+
+
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        res, cnt, start = 1, 1, 1
+        while start < len(chars):
+            if chars[start] != chars[start - 1]:
+                res += 1
+                while cnt > 1:
+                    cnt /= 10
+                    res += 1
+                start += 1
+                cnt = 1
+
+            else:
+                while start < len(chars) and chars[start] == chars[start - 1]:
+                    cnt += 1
+                    start += 1
+
+        while cnt > 1:
+            cnt /= 10
+            res += 1
+
+        return res
+
+
+if __name__ == '__main__':
+    print Solution().compress("aaaabbbbcce")
+    print Solution().compress("abcde")
+    print Solution().compress("aabbccceeeeeeee")
