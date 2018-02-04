dcfd2901a890c2f3538834170a41fb3b60539686
xin wei <xin_wei@intuit.com>
2018-02-04 06:12:56 -0800

    wrong answer:
    Input:
    [[-10,-8],[8,9],[-5,0],[6,10],[-6,-4],[1,7],[9,10],[-4,7]]
    Output:
    8
    Expected:
    4

---
+++ learnpythonthehardway/maximum-length-of-pair-chain.py
@@ -0,0 +1,38 @@
+# You are given n pairs of numbers. In every pair, the first number is always smaller than the second number.
+#
+# Now, we define a pair (c, d) can follow another pair (a, b) if and only if b < c. Chain of pairs can be formed in
+# this fashion.
+#
+# Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs.
+# You can select pairs in any order.
+#
+# Example 1:
+# Input: [[1,2], [2,3], [3,4]]
+# Output: 2
+# Explanation: The longest chain is [1,2] -> [3,4]
+# Note:
+# The number of given pairs will be in the range [1, 1000].
+
+import heapq
+
+
+class Solution(object):
+    def findLongestChain(self, pairs):
+        """
+        :type pairs: List[List[int]]
+        :rtype: int
+        """
+        pairs.sort()
+        heap, res = [], 0
+
+        for p in pairs:
+            while heap and p[1] < heap[0][0]:
+                heapq.heappop(heap)
+            if not heap or (heap and p[0] > heap[0][0]):
+                heapq.heappush(heap, (p[1], p))
+
+        return len(heap)
+
+
+if __name__ == '__main__':
+    print Solution().findLongestChain([[-10, -8], [8, 9], [-5, 0], [6, 10], [-6, -4], [1, 7], [9, 10], [-4, 7]])
