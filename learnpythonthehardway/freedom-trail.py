9359c84acbec1a2a9bcf30668d9e46405ceb2d22
xin wei <xin_wei@intuit.com>
2018-02-22 06:31:56 -0800

    Your input
    "godding"
    "gd"
    "godding"
    "gdi"
    "godding"
    "ggn"
    
    Your answer
    
    4
    8
    5
    
    Expected answer
    
    4
    7
    5
    
    this test case is not correct:
    
    "godding"
    "gdi"

---
+++ learnpythonthehardway/freedom-trail.py
@@ -0,0 +1,90 @@
+#  In the video game Fallout 4, the quest "Road to Freedom" requires players to reach a metal dial called the
+# "Freedom Trail Ring", and use the dial to spell a specific keyword in order to open the door.
+#
+# Given a string ring, which represents the code engraved on the outer ring and another string key, which represents
+# the keyword needs to be spelled. You need to find the minimum number of steps in order to spell all the characters
+# in the keyword.
+# Initially, the first character of the ring is aligned at 12:00 direction. You need to spell all the characters in
+# the string key one by one by rotating the ring clockwise or anticlockwise to make each character of the string key
+# aligned at 12:00 direction and then by pressing the center button.
+# At the stage of rotating the ring to spell the key character key[i]:
+#
+#     You can rotate the ring clockwise or anticlockwise one place, which counts as 1 step. The final purpose of the
+# rotation is to align one of the string ring's characters at the 12:00 direction, where this character must equal to
+#  the character key[i].
+#     If the character key[i] has been aligned at the 12:00 direction, you need to press the center button to spell,
+# which also counts as 1 step. After the pressing, you could begin to spell the next character in the key (next
+# stage), otherwise, you've finished all the spelling.
+#
+# Example:
+#
+#
+# https://leetcode.com/static/images/problemset/ring.jpg
+#
+# Note:
+#
+#     Length of both ring and key will be in range 1 to 100.
+#     There are only lowercase letters in both strings and might be some duplcate characters in both strings.
+#     It's guaranteed that string key could always be spelled by rotating the string ring.
+#
+#
+# Input: ring = "godding", key = "gd"
+# Output: 4
+# Explanation:
+#  For the first key character 'g', since it is already in place, we just need 1 step to spell this character.
+#  For the second key character 'd', we need to rotate the ring "godding" anticlockwise by two steps to make it
+# become "ddinggo".
+#  Also, we need 1 more step for spelling.
+#  So the final output is 4.
+import collections
+
+
+class Solution(object):
+    minStep = float('inf')
+
+    def findRotateSteps(self, ring, key):
+        """
+        :type ring: str
+        :type key: str
+        :rtype: int
+        """
+        dict = collections.defaultdict(set)
+
+        for k, v in enumerate(ring):
+            dict[v].add(k)
+
+        self.dfs(key, dict, 0, 0)
+
+        return self.minStep
+
+    def dfs(self, key, dict, pos, step):
+        if pos == len(key):
+            if step < self.minStep:
+                self.minStep = step
+            return
+
+        cSet = dict[key[pos]]
+        for p in cSet:
+            adjust = min(p, 7 - p)
+            tStep = step + adjust + 1
+            if tStep > self.minStep:  # prunning
+                continue
+            # https://stackoverflow.com/questions/2465921/how-to-copy-a-dictionary-and-only-edit-the-copy
+            dict2 = dict.copy()  # knowledge how to copy a dict
+            if adjust == p:  # rotate anti-clock
+                self.reset(key, dict2, -p)
+            else:
+                self.reset(key, dict2, len(key) - p)
+
+            self.dfs(key, dict2, pos + 1, tStep)
+
+    def reset(self, key, dict, adjust):
+        for k, v in dict.iteritems():
+            for s in v:
+                s = (s + adjust) % len(key)
+                s = s + len(key) if s < 0 else s
+
+
+if __name__ == '__main__':
+    print Solution().findRotateSteps("godding", "gd")
+    print Solution().findRotateSteps("godding", "gdi")
