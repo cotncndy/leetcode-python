de10057bb3825f9368330e4e484cb4e694d6c932
xin wei <xin_wei@intuit.com>
2018-02-25 19:57:04 -0800

    68 / 83 test cases passed. 	Status: Time Limit Exceeded
    Submitted: 0 minutes ago
    Last executed input: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
    
    longest palindrome subsequences

---
+++ learnpythonthehardway/largest-palindrome-subsequence.py
@@ -0,0 +1,52 @@
+#  Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length
+# of s is 1000.
+#
+# Example 1:
+# Input:
+#
+# "bbbab"
+#
+# Output:
+#
+# 4
+#
+# One possible longest palindromic subsequence is "bbbb".
+#
+# Example 2:
+# Input:
+#
+# "cbbd"
+#
+# Output:
+#
+# 2
+#
+# One possible longest palindromic subsequence is "bb".
+
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = len(s)
+        dp = [[0] * l for _ in xrange(l)]
+
+        for k in xrange(0, l):
+            for i in xrange(0, l):
+                if i + k == l:
+                    break
+                if k == 0:
+                    dp[i][i + k] = 1
+                else:
+                    if s[i] == s[i + k]:
+                        dp[i][i + k] = max(dp[i + 1][i + k - 1] + 2, dp[i][i + k - 1], dp[i + 1][i + k])
+                    else:
+                        dp[i][i + k] = max(dp[i][i + k - 1], dp[i + 1][i + k], dp[i + 1][i + k - 1])
+
+        return dp[0][-1]
+
+
+if __name__ == '__main__':
+    print Solution().longestPalindromeSubseq("bbbab")
+    print Solution().longestPalindromeSubseq("cbbd")
