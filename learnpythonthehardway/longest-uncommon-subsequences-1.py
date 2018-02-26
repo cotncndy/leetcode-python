995261aa7504f32ff8dcd4a9aedffb4e650dbf29
xin wei <xin_wei@intuit.com>
2018-02-26 07:07:04 -0800

    are you kidding me ?
    41 / 41 test cases passed.
    	Status: Accepted
    Runtime: 29 ms
    
    I don't get it.

--- learnpythonthehardway/longest-uncommon-subsequences-1.py
+++ learnpythonthehardway/longest-uncommon-subsequences-1.py
@@ -47,6 +47,9 @@ class Solution(object):
 
         return dp[-1][-1]
 
+    def findLUSlength2(self, a, b):
+        return -1 if a == b else max(len(a), len(b))
+
 
 if __name__ == '__main__':
     print Solution().findLUSlength("aba", "cdc")
