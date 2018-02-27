ea1397929dd2dfa8a84f05891730740c962dcfd5
xin wei <xin_wei@intuit.com>
2018-02-27 06:10:13 -0800

    98 / 98 test cases passed. 	Status: Accepted Runtime: 56 ms
    Submitted: 5 minutes ago
    
    but this problem is not solved by me, I basically don't understand it

--- learnpythonthehardway/longest-uncommon-subsequences-1.py
+++ learnpythonthehardway/longest-uncommon-subsequences-1.py
@@ -36,7 +36,7 @@ class Solution(object):
             for j in xrange(m):
                 if a[i] == a[j]:
                     if i == 0 or j == 0:
-                        dp[i][j] = 1
+                        dp[i][j] = 0
                     else:
                         dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
                 else:
