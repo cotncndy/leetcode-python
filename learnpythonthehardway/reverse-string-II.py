dea1c03238fab6bf66478232f140945f38d21501
xin wei <xin_wei@intuit.com>
2018-03-08 06:55:19 -0800

    39 / 60 test cases passed. Status: Wrong Answer Submitted: 0 minutes ago Input: "abcd" 4 Output: "abcd" Expected: "dcba"

---
+++ learnpythonthehardway/reverse-string-II.py
@@ -0,0 +1,26 @@
+# Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from
+# the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k
+# but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+# Example:
+# Input: s = "abcdefg", k = 2
+# Output: "bacdfeg"
+# Restrictions:
+# The string consists of lower English letters only.
+
+
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        right = k
+        while right < len(s):
+            s = s[:right - k] + s[right - k: right][::-1] + s[right:]
+            right += k * 2
+        return s
+
+
+if __name__ == '__main__':
+    print Solution().reverseStr("abcdefg", 2)
