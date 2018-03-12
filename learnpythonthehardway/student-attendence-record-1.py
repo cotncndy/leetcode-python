e37613cf8bd86787f7ba7120d43f22d8c1a76e0e
xin wei <xin_wei@intuit.com>
2018-03-12 05:57:37 -0700

    113 / 113 test cases passed. Status: Accepted Runtime: 44 ms, 18.99%

---
+++ learnpythonthehardway/student-attendence-record-1.py
@@ -0,0 +1,43 @@
+# You are given a string representing an attendance record for a student. The record only contains the following
+# three characters:
+# 'A' : Absent.
+# 'L' : Late.
+# 'P' : Present.
+# A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two
+# continuous 'L' (late).
+#
+# You need to return whether the student could be rewarded according to his attendance record.
+#
+# Example 1:
+# Input: "PPALLP"
+# Output: True
+# Example 2:
+# Input: "PPALLL"
+# Output: False
+
+
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        aCnt = 0
+
+        for k, v in enumerate(s):
+            if v == 'A':
+                aCnt += 1
+                if aCnt > 1:
+                    return False
+            if k < len(s) - 2 and v == 'L' and s[k + 1] == v and s[k + 2] == v:
+                return False
+
+        return True
+
+
+if __name__ == '__main__':
+    print Solution().checkRecord("PPALLP")
+    print Solution().checkRecord("PPALLL")
+    print Solution().checkRecord("APPLLPPLLPPLLL")
+    print Solution().checkRecord("APPLLPPLLPPLL")
+    print Solution().checkRecord("APPLLPPLLPPLLA")
