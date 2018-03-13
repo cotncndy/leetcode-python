11f6edf28025b8feafc3fe2f5fd19224536ffb3f
xin wei <xin_wei@intuit.com>
2018-03-13 06:35:50 -0700

    WA
    Your input
    [1000,100,10,2]
    Your answer
    "200"
    Expected answer
    "1000/(100/10/2)"

---
+++ learnpythonthehardway/optimal-divisioin.py
@@ -0,0 +1,63 @@
+# Given a list of positive integers, the adjacent integers will perform the float division. For example, [2,3,
+# 4] -> 2 / 3 / 4.
+#
+# However, you can add any number of parenthesis at any position to change the priority of operations. You should
+# find out how to add parenthesis to get the maximum result, and return the corresponding expression in string
+# format. Your expression should NOT contain redundant parenthesis.
+#
+# Example:
+# Input: [1000,100,10,2]
+# Output: "1000/(100/10/2)"
+# Explanation:
+# 1000/(100/10/2) = 1000/((100/10)/2) = 200
+# However, the bold parenthesis in "1000/((100/10)/2)" are redundant,
+# since they don't influence the operation priority. So you should return "1000/(100/10/2)".
+#
+# Other cases:
+# 1000/(100/10)/2 = 50
+# 1000/(100/(10/2)) = 50
+# 1000/100/10/2 = 0.5
+# 1000/100/(10/2) = 2
+# Note:
+#
+# The length of the input array is [1, 10].
+# Elements in the given array will be in range [2, 1000].
+# There is only one optimal division for each test case.
+import collections
+
+
+class Solution(object):
+    def optimalDivision(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        m = collections.defaultdict(set)
+        return str(max(self.dfs(nums, m, len(nums) - 1)))
+
+    def dfs(self, nums, m, n):
+        if n == 0:
+            m[0] = [nums[len(nums) - 1]]
+            return [nums[len(nums) - 1]]
+        if n in m:
+            return m[n]
+        res1, res = self.dfs(nums, m, n - 1), []
+        if n > 1:
+            res2 = self.dfs(nums, m, n - 2)
+            # res.append(nums[len(nums)-1-n] // nums[len(nums)-n] // i for i in res2)
+            for i in res2:
+                res.append(nums[len(nums) - 1 - n] // nums[len(nums) - n] // i)
+        for i in res1:
+            res.append(nums[len(nums) - 1 - n] // i)
+        # res.append(nums[len(nums)-1-n] // i for i in res1)
+
+        m[n] = res
+
+        return res
+
+
+if __name__ == '__main__':
+    print Solution().optimalDivision([2])
+    print Solution().optimalDivision([10, 2])
+    print Solution().optimalDivision([100, 10, 2])
+    print Solution().optimalDivision([1000, 100, 10, 2])
