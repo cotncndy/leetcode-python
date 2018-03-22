7903a43c9369bf31362907c744ad5463590ad35d
xin wei <xin_wei@intuit.com>
2018-03-22 06:06:53 -0700

    WA, should be 23, but it return 13

--- learnpythonthehardway/remove-boxed.py
+++ learnpythonthehardway/remove-boxed.py
@@ -56,6 +56,32 @@ class Solution(object):
 
         return (i + 1, j - 1)
 
+    def removeBoxes2(self, boxes):
+        """
+        :type boxes: List[int]
+        :rtype: int
+        """
+        # dp[100][100][100]
+        dp = [[[0] * 100 for _ in xrange(100)] for _ in xrange(100)]
+        return self.dfs(boxes, 0, len(boxes) - 1, 0, dp)
+
+    def dfs(self, boxes, l, r, k, dp):
+        if l > r:
+            return 0
+        # dp(l,r,k) mean there are consecutive k ele which == boxes[l] on l's left side
+        if dp[l][r][k]:
+            return dp[l][r][k]
+        dp[l][r][k] = (k + 1) ** 2 + self.dfs(boxes, l + 1, r, 0, dp)
+        for i in xrange(l + 1, r + 1):
+            if boxes[i] == boxes[l]:
+                dp[l][r][k] = max(dp[l][r][k],
+                                  self.dfs(boxes, l + 1, i - 1, 0, dp) + self.dfs(boxes, i + 1, r, k + 1, dp))
+
+        return dp[l][r][k]
+
+
+
+
 
 if __name__ == '__main__':
-    print Solution().removeBoxes([1, 3, 2, 2, 2, 4, 3, 1])
+    print Solution().removeBoxes2([1, 3, 2, 2, 2, 3, 4, 3, 1])
