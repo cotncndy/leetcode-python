f3035b6ee7fbddc4631b940bac5e933fc1693d51
xin wei <xin_wei@intuit.com>
2018-03-26 05:25:07 -0700

    WA, for this case:
    [3,4,5,1,2,null,null,null,0]
    [4,1,2]
    
        3
       / \
      4   5
     / \
    1   2
     \
      0
    
      4
     / \
    1   2
    
    it returns true

---
+++ learnpythonthehardway/subtree-of-another-tree.py
@@ -0,0 +1,80 @@
+# Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with
+# a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s
+# could also be considered as a subtree of itself.
+#
+# Example 1:
+# Given tree s:
+#
+#      3
+#     / \
+#    4   5
+#   / \
+#  1   2
+# Given tree t:
+#    4
+#   / \
+#  1   2
+# Return true, because t has the same structure and node values with a subtree of s.
+# Example 2:
+# Given tree s:
+#
+#      3
+#     / \
+#    4   5
+#   / \
+#  1   2
+#     /
+#    0
+# Given tree t:
+#    4
+#   / \
+#  1   2
+# Return false.
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+
+        def traverse(root, list):
+            if not root:
+                return ""
+            traverse(root.left, list)
+            traverse(root.right, list)
+            list += str(root.val)
+
+            return list
+
+        ls1, ls2 = [], []
+        ls1 = traverse(s, ls1)
+        ls2 = traverse(t, ls2)
+
+        st1 = "".join(map(str, ls1))
+        st2 = "".join(map(str, ls2))
+
+        if st2 in st1:
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    root, lN, rN = TreeNode(3), TreeNode(4), TreeNode(5)
+    root.left, root.right = lN, rN
+    llN, lrN = TreeNode(1), TreeNode(2)
+    lN.left, lN.right = llN, lrN
+
+    root2, lN1, rN1 = TreeNode(4), TreeNode(1), TreeNode(2)
+    root2.left, root2.right = lN1, rN1
+
+    print Solution().isSubtree(root, root2)
