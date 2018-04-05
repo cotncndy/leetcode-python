6dc35e91bee46e70a6e5f4fa64ecebcc583270c2
xin wei <xin_wei@intuit.com>
2018-04-05 06:07:15 -0700

    256 / 256 test cases passed. Status: Accepted Runtime: 52 ms, 24.14%

--- learnpythonthehardway/tag-validator.py
+++ learnpythonthehardway/tag-validator.py
@@ -126,5 +126,12 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-    # print Solution().isValid("<DIV>This is the first line <![CDATA[<div>]]></DIV>")
+    print Solution().isValid("<DIV>This is the first line <![CDATA[<div>]]></DIV>")
     print Solution().isValid("<DIV><></></DIV>")
+    print Solution().isValid("<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>")
+    print Solution().isValid("<A>  <B> </A>   </B>")
+    print Solution().isValid("<DIV>  div tag is not closed  <DIV>")
+    print Solution().isValid("<DIV>  unmatched <  </DIV>")
+    print Solution().isValid("<DIV> closed tags with invalid tag name  <b>123</b> </DIV>")
+    print Solution().isValid("<DIV> unmatched tags with invalid tag name  </1234567890> and <CDATA[[]]>  </DIV>")
+    print Solution().isValid("<DIV>  unmatched start tag <B>  and unmatched end tag </C>  </DIV>")
