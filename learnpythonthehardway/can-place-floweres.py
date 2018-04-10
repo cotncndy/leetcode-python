2cd9921463b9c4fe8ac441531d84b9f0d926d2ad
xin wei <xin_wei@intuit.com>
2018-04-10 07:06:55 -0700

    123 / 123 test cases passed. Status: Accepted Runtime: 58 ms, 88.17%

--- learnpythonthehardway/can-place-floweres.py
+++ learnpythonthehardway/can-place-floweres.py
@@ -32,20 +32,25 @@ class Solution(object):
         if len(flowerbed) == 1 and flowerbed[0] == 1:
             return False
 
+        if flowerbed[0] == 0 and flowerbed[1] == 0:
+            n, flowerbed[0] = n - 1, 1
+            if n == 0:
+                return True
+
+        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+            n, flowerbed[-1] = n - 1, 1
+            if n == 0:
+                return True
+
         for i in xrange(1, len(flowerbed) - 1):
             if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
                 n, flowerbed[i] = n - 1, 1
                 if n == 0:
                     return True
 
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            n -= 1
-            if n == 0:
-                return True
-
-        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
-            n -= 1
-            if n == 0:
-                return True
 
         return False
+
+
+if __name__ == '__main__':
+    print Solution().canPlaceFlowers([0, 0, 0, 0, 1], 2)
