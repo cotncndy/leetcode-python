6870e9b9b624ac421eb97d7343da37dec60da751
xin wei <xin_wei@intuit.com>
2018-04-15 17:42:28 -0700

    220 / 220 test cases passed. Status: Accepted Runtime: 2267 ms, 8.74%

--- learnpythonthehardway/valid-triangle.py
+++ learnpythonthehardway/valid-triangle.py
@@ -30,6 +30,28 @@ class Solution(object):
 
         return res
 
+    def triangleNumber2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        res = 0
+        for l1 in xrange(len(nums)-2):
+            for l2 in xrange(l1+1,len(nums)-1):
+                s, left, right = nums[l1] + nums[l2], l2+1, len(nums)
+                while left < right:
+                    mid = left + (right-left) / 2
+                    if nums[mid] < s:
+                        left = mid + 1
+                    else:
+                        right = mid
+
+                res += right - l2 - 1
+
+
+        return res
+
 if __name__ == '__main__':
     print Solution().triangleNumber([2,2,3,4])
     print Solution().triangleNumber([1,2,3,4,5,6])
