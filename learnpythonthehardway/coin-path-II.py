e18c844d415c35fa5d2bdc8d50f27b1beee4263d
xin wei <xin_wei@intuit.com>
2018-05-02 12:25:18 -0700

    226 / 226 test cases passed. Status: Accepted Runtime: 282 ms, 40%

--- learnpythonthehardway/coin-path-II.py
+++ learnpythonthehardway/coin-path-II.py
@@ -43,16 +43,16 @@ class Solution(object):
         for i in xrange(m):
             if A[i] == -1:
                 continue
-            for j in xrange(1,B+1):
+            for j in xrange(1, B + 1):
                 if i + j >= m:
                     break
-                if A[i+j] != -1:
+                if A[i + j] != -1:
                     cost = dp[i][0][0]
-                    if cost + A[i+j] < dp[i+j][0][0]:
-                        del dp[i+j][:] # clear the list
-                        dp[i+j].append((cost + A[i+j], i))
-                    elif cost + A[i+j] == dp[i+j][0][0]:
-                        dp[i+j].append((cost + A[i+j], i))
+                    if cost + A[i + j] < dp[i + j][0][0]:
+                        del dp[i + j][:]  # clear the list
+                        dp[i + j].append((cost + A[i + j], i))
+                    elif cost + A[i + j] == dp[i + j][0][0]:
+                        dp[i + j].append((cost + A[i + j], i))
 
         if dp[-1][0][0] == float('inf'):
             return []
@@ -60,7 +60,7 @@ class Solution(object):
         # print dp
         li = [m]
         self.res = []
-        self.backtrack(dp, m-1, li)
+        self.backtrack(dp, m - 1, li)
         print self.res
         self.res.sort()
         print self.res
@@ -71,14 +71,12 @@ class Solution(object):
             self.res.append(li[::-1])
             return
 
-        #[[0]]  [[0,2],[0,3]]
+        # [[0]]  [[0,2],[0,3]]
         for p in dp[pos]:
             tmp = []
             tmp.extend(li)
-            tmp.append(p[1]+1)
-            self.backtrack(dp,p[1], tmp )
-
-
+            tmp.append(p[1] + 1)
+            self.backtrack(dp, p[1], tmp)
 
     def cheapestJump2(self, A, B):
         """
@@ -88,34 +86,33 @@ class Solution(object):
         """
 
         m = len(A)
-        if A[0] == -1 or A[m-1]  == -1:
+        if A[0] == -1 or A[m - 1] == -1:
             return []
-        dp = [(float('inf'), -1)]  * m
-        dp[m-1] = (A[m-1], -1)
+        dp = [(float('inf'), -1)] * m
+        dp[m - 1] = (A[m - 1], -1)
 
-        for i in xrange(m-2,-1,-1):
+        for i in xrange(m - 2, -1, -1):
             if A[i] == -1:
                 continue
-            j = i+1
-            while j <= min(i+B,m-1):
+            j = i + 1
+            while j <= min(i + B, m - 1):
                 if dp[j][0] == float('inf'):
-                    j += 1 # bugfixed
+                    j += 1  # bugfixed
                     continue
                 cost = dp[j][0] + A[i]
                 if cost < dp[i][0]:
                     dp[i] = (cost, j)
                 j += 1
 
-
         if dp[0][0] == float('inf'):
             return []
         # print dp
-        i, res =0, [1]
+        i, res = 0, [1]
         while i < m:
             i = dp[i][1]
             if i == -1:
                 break
-            res.append(i+1)
+            res.append(i + 1)
         return res
 
     def cheapestJump3(self, A, B):
@@ -126,40 +123,45 @@ class Solution(object):
         """
 
         m = len(A)
-        if A[0] == -1 or A[m-1]  == -1:
+        if A[0] == -1 or A[m - 1] == -1:
             return []
-        dp = [(float('inf'), -1)]  * m
+        dp = [(float('inf'), -1)] * m
         dp[0] = (A[0], -1)
+        leng = [0] * m
 
         for i in xrange(m):
             if A[i] == -1:
                 continue
-            j = max(i-B,0)
+            j = max(i - B, 0)
             while j < i:
                 if dp[j][0] == float('inf'):
-                    j += 1 # bugfixed
+                    j += 1  # bugfixed
                     continue
                 cost = dp[j][0] + A[i]
-                if cost <= dp[i][0]:
+                if cost < dp[i][0]  or (cost == dp[i][0] and leng[i] < leng[j] + 1):
                     dp[i] = (cost, j)
+                    leng[i] = leng[j] + 1
                 j += 1
-
 
         if dp[-1][0] == float('inf'):
             return []
-        # print dp
-        i, res =m-1, [m]
+        print dp
+        i, res = m - 1, [m]
         while i >= 0:
             i = dp[i][1]
             if i == -1:
                 break
-            res.append(i+1)
+            res.append(i + 1)
         return res[::-1]
 
+
 if __name__ == '__main__':
-
-    print Solution().cheapestJump3([1,2,4,-1,2],2)
-    print Solution().cheapestJump3([1,2,4,-1,2],1)
-    print Solution().cheapestJump3([0,0,0,0,0,0],3)
-    print Solution().cheapestJump3([0,-1,-1,-1,-1,-1],6)
-
+    # print Solution().cheapestJump3([1, 2, 4, -1, 2], 2)
+    # print Solution().cheapestJump3([1, 2, 4, -1, 2], 1)
+    # print Solution().cheapestJump3([0, 0, 0, 0, 0, 0], 3)
+    # print Solution().cheapestJump3([0, -1, -1, -1, -1, -1], 6)
+    print Solution().cheapestJump3(
+        [33, 90, 57, 39, 42, 45, 29, 90, 81, 87, 88, 37, 58, 97, 80, 2, 77, 64, 82, 41, 2, 33, 34, 95, 85, 77, 92, 3, 8,
+         15, 71, 84, 58, 65, 46, 48, 3, 74, 4, 83, 23, 12, 15, 77, 33, 65, 17, 86, 21, 62, 71, 55, 80, 63, 75, 55, 11,
+         34, -1, 64, 81, 18, 77, 82, 8, 12, 14, 6, 46, 39, 71, 14, 6, 46, 89, 37, 88, 70, 88, 33, 89, 92, 60, 0, 78, 10,
+         88, 99, 20], 74)
